22e8cf8c6b6d93c3ae016d82603124eba27a1784
zhangxin <zhangxin19870504@163.com>
2020-06-15 20:05:34 +0800

    修改显示方式，微调显示参数

--- tools/infer/predict_system.py
+++ tools/infer/predict_system.py
@@ -134,9 +134,8 @@ if __name__ == "__main__":
             txts = [rec_res[i][0] for i in range(len(rec_res))]
             scores = [rec_res[i][1] for i in range(len(rec_res))]
 
-            # draw_img = draw_ocr(
-            #     image, boxes, txts, scores, draw_txt=True, drop_score=0.5)
-            draw_img = draw_ocr_box_txt(image, boxes, txts)
+            draw_img = draw_ocr(
+                image, boxes, txts, scores, draw_txt=True, drop_score=0.5)
             draw_img_save = "./inference_results/"
             if not os.path.exists(draw_img_save):
                 os.makedirs(draw_img_save)
