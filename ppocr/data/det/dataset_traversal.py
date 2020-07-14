52e7dbf77317b99cc909b11a027f6457e75b891e
LDOUBLEV <liuvv0203@outlook.com>
2020-07-14 15:48:43 +0800

    add continue in det Evalreader when img is not exist

--- ppocr/data/det/dataset_traversal.py
+++ ppocr/data/det/dataset_traversal.py
@@ -96,6 +96,7 @@ class EvalTestReader(object):
                 img = cv2.imread(img_path)
                 if img is None:
                     logger.info("{} does not exist!".format(img_path))
+                    continue
                 elif len(list(img.shape)) == 2 or img.shape[2] == 1:
                     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
                 outs = process_function(img)
