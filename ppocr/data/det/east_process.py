c12e9ec564f8dee45df331f5bf582373c54c1d39
shaohua.zhang <BeyondYourself@users.noreply.github.com>
2020-08-03 21:47:25 +0800

    Update east_process.py

--- ppocr/data/det/east_process.py
+++ ppocr/data/det/east_process.py
@@ -17,6 +17,7 @@ import cv2
 import numpy as np
 import json
 import sys
+import os
 
 class EASTProcessTrain(object):
     def __init__(self, params):
@@ -52,7 +53,7 @@ class EASTProcessTrain(object):
         label_infor = label_infor.decode()
         label_infor = label_infor.encode('utf-8').decode('utf-8-sig')
         substr = label_infor.strip("\n").split("\t")
-        img_path = self.img_set_dir + "/" + substr[0]
+        img_path = os.path.join(self.img_set_dir, substr[0])
         label = json.loads(substr[1])
         nBox = len(label)
         wordBBs, txts, txt_tags = [], [], []
