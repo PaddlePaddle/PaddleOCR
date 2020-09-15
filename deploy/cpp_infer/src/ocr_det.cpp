5858441b3d95c15ba040c061ae968f1e19f879b7
littletomatodonkey <dazhiningsibuqu@163.com>
2020-09-15 15:52:52 +0000

    add notes

--- deploy/cpp_infer/src/ocr_det.cpp
+++ deploy/cpp_infer/src/ocr_det.cpp
@@ -26,6 +26,7 @@ void DBDetector::LoadModel(const std::string &model_dir) {
     config.DisableGpu();
     if (this->use_mkldnn_) {
       config.EnableMKLDNN();
+      // cache 10 different shapes for mkldnn to avoid memory leak
       config.SetMkldnnCacheCapacity(10);
     }
     config.SetCpuMathLibraryNumThreads(this->cpu_math_library_num_threads_);
