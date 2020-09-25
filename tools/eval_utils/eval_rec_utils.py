fc512a84e3ea1a49911f83459c6ac57699009304
tink2123 <y_tink@163.com>
2020-09-25 16:55:56 +0800

    add anno for rec

--- tools/eval_utils/eval_rec_utils.py
+++ tools/eval_utils/eval_rec_utils.py
@@ -122,7 +122,9 @@ def eval_rec_run(exe, config, eval_info_dict, mode):
 
 
 def test_rec_benchmark(exe, config, eval_info_dict):
-    " Evaluate lmdb dataset "
+    """
+    eval rec benchmark
+    """
     eval_data_list = ['IIIT5k_3000', 'SVT', 'IC03_860', 'IC03_867', \
                       'IC13_857', 'IC13_1015', 'IC15_1811', 'IC15_2077', 'SVTP', 'CUTE80']
     eval_data_dir = config['TestReader']['lmdb_sets_dir']
