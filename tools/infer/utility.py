6241b8f9ca0c17933d91646dc0ab4c9827760015
WenmuZhou <zjwenmu@gmail.com>
2020-10-22 19:03:24 +0800

    添加静态图的create_predictor

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -85,8 +85,8 @@ def create_predictor(args, mode, logger):
     if model_dir is None:
         logger.info("not find {} model file path {}".format(mode, model_dir))
         sys.exit(0)
-    model_file_path = model_dir + "/__model__"
-    params_file_path = model_dir + "/__variables__"
+    model_file_path = model_dir + "/model"
+    params_file_path = model_dir + "/params"
     if not os.path.exists(model_file_path):
         logger.info("not find model file path {}".format(model_file_path))
         sys.exit(0)
