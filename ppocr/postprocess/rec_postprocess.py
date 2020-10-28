e6c3f952bcf6d456c3b7f3e90ce4ba1a2ec797fb
WenmuZhou <zjwenmu@gmail.com>
2020-10-28 15:51:31 +0800

    适配新的网络

--- ppocr/postprocess/rec_postprocess.py
+++ ppocr/postprocess/rec_postprocess.py
@@ -100,9 +100,10 @@ class CTCLabelDecode(BaseRecLabelDecode):
                                              character_type, use_space_char)
 
     def __call__(self, preds, label=None, *args, **kwargs):
+        if isinstance(preds, paddle.Tensor):
+            preds = preds.numpy()
         # out = self.decode_preds(preds)
 
-        preds = F.softmax(preds, axis=2).numpy()
         preds_idx = preds.argmax(axis=2)
         preds_prob = preds.max(axis=2)
         text = self.decode(preds_idx, preds_prob)
@@ -116,8 +117,7 @@ class CTCLabelDecode(BaseRecLabelDecode):
         return dict_character
 
     def decode_preds(self, preds):
-        probs = F.softmax(preds, axis=2).numpy()
-        probs_ind = np.argmax(probs, axis=2)
+        probs_ind = np.argmax(preds, axis=2)
 
         B, N, _ = preds.shape
         l = np.ones(B).astype(np.int64) * N
@@ -128,7 +128,7 @@ class CTCLabelDecode(BaseRecLabelDecode):
         ]
 
         result_list = []
-        for sample_idx, ind, prob in zip(batch_res, probs_ind, probs):
+        for sample_idx, ind, prob in zip(batch_res, probs_ind, preds):
             char_list = [self.character[idx] for idx in sample_idx]
             valid_ind = np.where(ind != 0)[0]
             if len(valid_ind) == 0:
