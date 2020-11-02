05bbf9a87440eba2120ca30e9ef9b4e40e9c29a1
Zacks Tsang <zshaojia@gmail.com>
2020-11-02 19:22:48 +0800

    modify character special

--- ppocr/utils/special_character.py
+++ ppocr/utils/special_character.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#coding=utf-8
-import string
-
 class SpecialCharacter(object):
     """
     Special Sign Converter
