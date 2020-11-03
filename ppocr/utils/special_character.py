e8216d5b2df4c9ef614e08a17be3f005e36fdd1a
Zacks Tsang <zshaojia@gmail.com>
2020-11-03 14:29:15 +0800

    fix special character bug

--- ppocr/utils/special_character.py
+++ ppocr/utils/special_character.py
@@ -22,17 +22,17 @@ class SpecialCharacter(object):
         self.special_char = []
         self.normal_char = []
         if "special_character_dict_path" in config: 
-            special_char_dict_path = config["special_character_dict_path"]
+            special_char_dict_path = config['special_character_dict_path']
             with open(special_char_dict_path, "rb") as fin:
                 lines = fin.readlines()
                 for line in lines:
-                    line = line.strip("\n").strip("\r\n")
+                    line = line.decode('utf-8').strip("\n").strip("\r\n")
                     result = line.split(',')
                     if len(result) == 2:
                         self.special_char.append(result[0])
                         self.normal_char.append(result[1])
         else:
-            self.special_char = ['０','１','２','３','４','５','６','７','８','９','Ａ','Ｂ','Ｃ','Ｄ','Ｅ','Ｆ','Ｇ','Ｈ','Ｉ','Ｊ','Ｋ','Ｌ','Ｍ','Ｎ','Ｏ','Ｐ','Ｑ','Ｒ','Ｓ','Ｔ','Ｕ','Ｖ','Ｗ','Ｘ','Ｙ','Ｚ']
+            self.special_char = [u'０',u'１',u'２',u'３',u'４',u'５',u'６',u'７',u'８',u'９',u'Ａ',u'Ｂ',u'Ｃ',u'Ｄ',u'Ｅ',u'Ｆ',u'Ｇ',u'Ｈ',u'Ｉ',u'Ｊ',u'Ｋ',u'Ｌ',u'Ｍ',u'Ｎ',u'Ｏ',u'Ｐ',u'Ｑ',u'Ｒ',u'Ｓ',u'Ｔ',u'Ｕ',u'Ｖ',u'Ｗ',u'Ｘ',u'Ｙ',u'Ｚ']
             self.normal_char = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
     def normalText(self, text):
@@ -40,7 +40,6 @@ class SpecialCharacter(object):
         normal converter, replace special sign
         """
         for index,item in enumerate(self.special_char):
-            item = item.decode('utf-8')
             if text.find(item) >= 0:
                 text = text.replace(item, self.normal_char[index])
 
