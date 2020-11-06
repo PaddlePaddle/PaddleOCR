6e4249eaf2b342275b73247bfe5fc515b17d7850
WenmuZhou <zjwenmu@gmail.com>
2020-11-06 19:12:34 +0800

    change python to python3

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
- python -m paddle.distributed.launch --selected_gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/det/det_mv3_db.yml
+ python3 -m paddle.distributed.launch --selected_gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc_lmdb_local.yml
