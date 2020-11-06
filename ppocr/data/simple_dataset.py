60f8f1e1812cb6e45f0d54e41bcf8c33e31ea3b1
WenmuZhou <zjwenmu@gmail.com>
2020-11-06 19:09:52 +0800

    del unuse import

--- ppocr/data/simple_dataset.py
+++ ppocr/data/simple_dataset.py
@@ -11,13 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import copy
 import numpy as np
 import os
 import random
-import paddle
 from paddle.io import Dataset
-import time
 
 from .imaug import transform, create_operators
 
