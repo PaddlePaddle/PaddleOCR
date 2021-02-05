c3bb6b5228d0ae607783fca1a839b39c3b28e72f
WenmuZhou <zjwenmu@gmail.com>
2021-02-05 12:27:24 +0800

    Android demo init

---
+++ deploy/android_demo/app/src/main/cpp/ocr_ppredictor.cpp
@@ -0,0 +1,261 @@
+//
+// Created by fujiayi on 2020/7/1.
+//
+
+#include "ocr_ppredictor.h"
+#include "common.h"
+#include "ocr_cls_process.h"
+#include "ocr_crnn_process.h"
+#include "ocr_db_post_process.h"
+#include "preprocess.h"
+
+namespace ppredictor {
+
+OCR_PPredictor::OCR_PPredictor(const OCR_Config &config) : _config(config) {}
+
+int OCR_PPredictor::init(const std::string &det_model_content,
+                         const std::string &rec_model_content,
+                         const std::string &cls_model_content) {
+  _det_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR, _config.mode});
+  _det_predictor->init_nb(det_model_content);
+
+  _rec_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR_INTERNAL, _config.mode});
+  _rec_predictor->init_nb(rec_model_content);
+
+  _cls_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR_INTERNAL, _config.mode});
+  _cls_predictor->init_nb(cls_model_content);
+  return RETURN_OK;
+}
+
+int OCR_PPredictor::init_from_file(const std::string &det_model_path,
+                                   const std::string &rec_model_path,
+                                   const std::string &cls_model_path) {
+  _det_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR, _config.mode});
+  _det_predictor->init_from_file(det_model_path);
+
+  _rec_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR_INTERNAL, _config.mode});
+  _rec_predictor->init_from_file(rec_model_path);
+
+  _cls_predictor = std::unique_ptr<PPredictor>(
+      new PPredictor{_config.thread_num, NET_OCR_INTERNAL, _config.mode});
+  _cls_predictor->init_from_file(cls_model_path);
+  return RETURN_OK;
+}
+/**
+ * for debug use, show result of First Step
+ * @param filter_boxes
+ * @param boxes
+ * @param srcimg
+ */
+static void
+visual_img(const std::vector<std::vector<std::vector<int>>> &filter_boxes,
+           const std::vector<std::vector<std::vector<int>>> &boxes,
+           const cv::Mat &srcimg) {
+  // visualization
+  cv::Point rook_points[filter_boxes.size()][4];
+  for (int n = 0; n < filter_boxes.size(); n++) {
+    for (int m = 0; m < filter_boxes[0].size(); m++) {
+      rook_points[n][m] =
+          cv::Point(int(filter_boxes[n][m][0]), int(filter_boxes[n][m][1]));
+    }
+  }
+
+  cv::Mat img_vis;
+  srcimg.copyTo(img_vis);
+  for (int n = 0; n < boxes.size(); n++) {
+    const cv::Point *ppt[1] = {rook_points[n]};
+    int npt[] = {4};
+    cv::polylines(img_vis, ppt, npt, 1, 1, CV_RGB(0, 255, 0), 2, 8, 0);
+  }
+  // 调试用，自行替换需要修改的路径
+  cv::imwrite("/sdcard/1/vis.png", img_vis);
+}
+
+std::vector<OCRPredictResult>
+OCR_PPredictor::infer_ocr(const std::vector<int64_t> &dims,
+                          const float *input_data, int input_len, int net_flag,
+                          cv::Mat &origin) {
+  PredictorInput input = _det_predictor->get_first_input();
+  input.set_dims(dims);
+  input.set_data(input_data, input_len);
+  std::vector<PredictorOutput> results = _det_predictor->infer();
+  PredictorOutput &res = results.at(0);
+  std::vector<std::vector<std::vector<int>>> filtered_box = calc_filtered_boxes(
+      res.get_float_data(), res.get_size(), (int)dims[2], (int)dims[3], origin);
+  LOGI("Filter_box size %ld", filtered_box.size());
+  return infer_rec(filtered_box, origin);
+}
+
+std::vector<OCRPredictResult> OCR_PPredictor::infer_rec(
+    const std::vector<std::vector<std::vector<int>>> &boxes,
+    const cv::Mat &origin_img) {
+  std::vector<float> mean = {0.5f, 0.5f, 0.5f};
+  std::vector<float> scale = {1 / 0.5f, 1 / 0.5f, 1 / 0.5f};
+  std::vector<int64_t> dims = {1, 3, 0, 0};
+  std::vector<OCRPredictResult> ocr_results;
+
+  PredictorInput input = _rec_predictor->get_first_input();
+  for (auto bp = boxes.crbegin(); bp != boxes.crend(); ++bp) {
+    const std::vector<std::vector<int>> &box = *bp;
+    cv::Mat crop_img = get_rotate_crop_image(origin_img, box);
+    crop_img = infer_cls(crop_img);
+
+    float wh_ratio = float(crop_img.cols) / float(crop_img.rows);
+    cv::Mat input_image = crnn_resize_img(crop_img, wh_ratio);
+    input_image.convertTo(input_image, CV_32FC3, 1 / 255.0f);
+    const float *dimg = reinterpret_cast<const float *>(input_image.data);
+    int input_size = input_image.rows * input_image.cols;
+
+    dims[2] = input_image.rows;
+    dims[3] = input_image.cols;
+    input.set_dims(dims);
+
+    neon_mean_scale(dimg, input.get_mutable_float_data(), input_size, mean,
+                    scale);
+
+    std::vector<PredictorOutput> results = _rec_predictor->infer();
+    const float *predict_batch = results.at(0).get_float_data();
+    const std::vector<int64_t> predict_shape = results.at(0).get_shape();
+
+    OCRPredictResult res;
+
+    // ctc decode
+    int argmax_idx;
+    int last_index = 0;
+    float score = 0.f;
+    int count = 0;
+    float max_value = 0.0f;
+
+    for (int n = 0; n < predict_shape[1]; n++) {
+      argmax_idx = int(argmax(&predict_batch[n * predict_shape[2]],
+                              &predict_batch[(n + 1) * predict_shape[2]]));
+      max_value =
+          float(*std::max_element(&predict_batch[n * predict_shape[2]],
+                                  &predict_batch[(n + 1) * predict_shape[2]]));
+      if (argmax_idx > 0 && (!(n > 0 && argmax_idx == last_index))) {
+        score += max_value;
+        count += 1;
+        res.word_index.push_back(argmax_idx);
+      }
+      last_index = argmax_idx;
+    }
+    score /= count;
+    if (res.word_index.empty()) {
+      continue;
+    }
+    res.score = score;
+    res.points = box;
+    ocr_results.emplace_back(std::move(res));
+  }
+  LOGI("ocr_results finished %lu", ocr_results.size());
+  return ocr_results;
+}
+
+cv::Mat OCR_PPredictor::infer_cls(const cv::Mat &img, float thresh) {
+  std::vector<float> mean = {0.5f, 0.5f, 0.5f};
+  std::vector<float> scale = {1 / 0.5f, 1 / 0.5f, 1 / 0.5f};
+  std::vector<int64_t> dims = {1, 3, 0, 0};
+  std::vector<OCRPredictResult> ocr_results;
+
+  PredictorInput input = _cls_predictor->get_first_input();
+
+  cv::Mat input_image = cls_resize_img(img);
+  input_image.convertTo(input_image, CV_32FC3, 1 / 255.0f);
+  const float *dimg = reinterpret_cast<const float *>(input_image.data);
+  int input_size = input_image.rows * input_image.cols;
+
+  dims[2] = input_image.rows;
+  dims[3] = input_image.cols;
+  input.set_dims(dims);
+
+  neon_mean_scale(dimg, input.get_mutable_float_data(), input_size, mean,
+                  scale);
+
+  std::vector<PredictorOutput> results = _cls_predictor->infer();
+
+  const float *scores = results.at(0).get_float_data();
+  float score = 0;
+  int label = 0;
+  for (int64_t i = 0; i < results.at(0).get_size(); i++) {
+    LOGI("output scores [%f]", scores[i]);
+    if (scores[i] > score) {
+      score = scores[i];
+      label = i;
+    }
+  }
+  cv::Mat srcimg;
+  img.copyTo(srcimg);
+  if (label % 2 == 1 && score > thresh) {
+    cv::rotate(srcimg, srcimg, 1);
+  }
+  return srcimg;
+}
+
+std::vector<std::vector<std::vector<int>>>
+OCR_PPredictor::calc_filtered_boxes(const float *pred, int pred_size,
+                                    int output_height, int output_width,
+                                    const cv::Mat &origin) {
+  const double threshold = 0.3;
+  const double maxvalue = 1;
+
+  cv::Mat pred_map = cv::Mat::zeros(output_height, output_width, CV_32F);
+  memcpy(pred_map.data, pred, pred_size * sizeof(float));
+  cv::Mat cbuf_map;
+  pred_map.convertTo(cbuf_map, CV_8UC1);
+
+  cv::Mat bit_map;
+  cv::threshold(cbuf_map, bit_map, threshold, maxvalue, cv::THRESH_BINARY);
+
+  std::vector<std::vector<std::vector<int>>> boxes =
+      boxes_from_bitmap(pred_map, bit_map);
+  float ratio_h = output_height * 1.0f / origin.rows;
+  float ratio_w = output_width * 1.0f / origin.cols;
+  std::vector<std::vector<std::vector<int>>> filter_boxes =
+      filter_tag_det_res(boxes, ratio_h, ratio_w, origin);
+  return filter_boxes;
+}
+
+std::vector<int>
+OCR_PPredictor::postprocess_rec_word_index(const PredictorOutput &res) {
+  const int *rec_idx = res.get_int_data();
+  const std::vector<std::vector<uint64_t>> rec_idx_lod = res.get_lod();
+
+  std::vector<int> pred_idx;
+  for (int n = int(rec_idx_lod[0][0]); n < int(rec_idx_lod[0][1] * 2); n += 2) {
+    pred_idx.emplace_back(rec_idx[n]);
+  }
+  return pred_idx;
+}
+
+float OCR_PPredictor::postprocess_rec_score(const PredictorOutput &res) {
+  const float *predict_batch = res.get_float_data();
+  const std::vector<int64_t> predict_shape = res.get_shape();
+  const std::vector<std::vector<uint64_t>> predict_lod = res.get_lod();
+  int blank = predict_shape[1];
+  float score = 0.f;
+  int count = 0;
+  for (int n = predict_lod[0][0]; n < predict_lod[0][1] - 1; n++) {
+    int argmax_idx = argmax(predict_batch + n * predict_shape[1],
+                            predict_batch + (n + 1) * predict_shape[1]);
+    float max_value = predict_batch[n * predict_shape[1] + argmax_idx];
+    if (blank - 1 - argmax_idx > 1e-5) {
+      score += max_value;
+      count += 1;
+    }
+  }
+  if (count == 0) {
+    LOGE("calc score count 0");
+  } else {
+    score /= count;
+  }
+  LOGI("calc score: %f", score);
+  return score;
+}
+
+NET_TYPE OCR_PPredictor::get_net_flag() const { return NET_OCR; }
+}
---
+++ deploy/android_demo/app/src/main/java/com/baidu/paddle/lite/demo/ocr/Predictor.java
@@ -0,0 +1,356 @@
+package com.baidu.paddle.lite.demo.ocr;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.Log;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+import static android.graphics.Color.*;
+
+public class Predictor {
+    private static final String TAG = Predictor.class.getSimpleName();
+    public boolean isLoaded = false;
+    public int warmupIterNum = 1;
+    public int inferIterNum = 1;
+    public int cpuThreadNum = 4;
+    public String cpuPowerMode = "LITE_POWER_HIGH";
+    public String modelPath = "";
+    public String modelName = "";
+    protected OCRPredictorNative paddlePredictor = null;
+    protected float inferenceTime = 0;
+    // Only for object detection
+    protected Vector<String> wordLabels = new Vector<String>();
+    protected String inputColorFormat = "BGR";
+    protected long[] inputShape = new long[]{1, 3, 960};
+    protected float[] inputMean = new float[]{0.485f, 0.456f, 0.406f};
+    protected float[] inputStd = new float[]{1.0f / 0.229f, 1.0f / 0.224f, 1.0f / 0.225f};
+    protected float scoreThreshold = 0.1f;
+    protected Bitmap inputImage = null;
+    protected Bitmap outputImage = null;
+    protected volatile String outputResult = "";
+    protected float preprocessTime = 0;
+    protected float postprocessTime = 0;
+
+
+    public Predictor() {
+    }
+
+    public boolean init(Context appCtx, String modelPath, String labelPath) {
+        isLoaded = loadModel(appCtx, modelPath, cpuThreadNum, cpuPowerMode);
+        if (!isLoaded) {
+            return false;
+        }
+        isLoaded = loadLabel(appCtx, labelPath);
+        return isLoaded;
+    }
+
+
+    public boolean init(Context appCtx, String modelPath, String labelPath, int cpuThreadNum, String cpuPowerMode,
+                        String inputColorFormat,
+                        long[] inputShape, float[] inputMean,
+                        float[] inputStd, float scoreThreshold) {
+        if (inputShape.length != 3) {
+            Log.e(TAG, "Size of input shape should be: 3");
+            return false;
+        }
+        if (inputMean.length != inputShape[1]) {
+            Log.e(TAG, "Size of input mean should be: " + Long.toString(inputShape[1]));
+            return false;
+        }
+        if (inputStd.length != inputShape[1]) {
+            Log.e(TAG, "Size of input std should be: " + Long.toString(inputShape[1]));
+            return false;
+        }
+        if (inputShape[0] != 1) {
+            Log.e(TAG, "Only one batch is supported in the image classification demo, you can use any batch size in " +
+                    "your Apps!");
+            return false;
+        }
+        if (inputShape[1] != 1 && inputShape[1] != 3) {
+            Log.e(TAG, "Only one/three channels are supported in the image classification demo, you can use any " +
+                    "channel size in your Apps!");
+            return false;
+        }
+        if (!inputColorFormat.equalsIgnoreCase("BGR")) {
+            Log.e(TAG, "Only  BGR color format is supported.");
+            return false;
+        }
+        boolean isLoaded = init(appCtx, modelPath, labelPath);
+        if (!isLoaded) {
+            return false;
+        }
+        this.inputColorFormat = inputColorFormat;
+        this.inputShape = inputShape;
+        this.inputMean = inputMean;
+        this.inputStd = inputStd;
+        this.scoreThreshold = scoreThreshold;
+        return true;
+    }
+
+    protected boolean loadModel(Context appCtx, String modelPath, int cpuThreadNum, String cpuPowerMode) {
+        // Release model if exists
+        releaseModel();
+
+        // Load model
+        if (modelPath.isEmpty()) {
+            return false;
+        }
+        String realPath = modelPath;
+        if (!modelPath.substring(0, 1).equals("/")) {
+            // Read model files from custom path if the first character of mode path is '/'
+            // otherwise copy model to cache from assets
+            realPath = appCtx.getCacheDir() + "/" + modelPath;
+            Utils.copyDirectoryFromAssets(appCtx, modelPath, realPath);
+        }
+        if (realPath.isEmpty()) {
+            return false;
+        }
+
+        OCRPredictorNative.Config config = new OCRPredictorNative.Config();
+        config.cpuThreadNum = cpuThreadNum;
+        config.detModelFilename = realPath + File.separator + "ch_det_mv3_db_opt.nb";
+        config.recModelFilename = realPath + File.separator + "ch_rec_mv3_crnn_opt.nb";
+        config.clsModelFilename = realPath + File.separator + "ch_cls_mv3_opt.nb";
+        Log.e("Predictor", "model path" + config.detModelFilename + " ; " + config.recModelFilename + ";" + config.clsModelFilename);
+        config.cpuPower = cpuPowerMode;
+        paddlePredictor = new OCRPredictorNative(config);
+
+        this.cpuThreadNum = cpuThreadNum;
+        this.cpuPowerMode = cpuPowerMode;
+        this.modelPath = realPath;
+        this.modelName = realPath.substring(realPath.lastIndexOf("/") + 1);
+        return true;
+    }
+
+    public void releaseModel() {
+        if (paddlePredictor != null) {
+            paddlePredictor.destory();
+            paddlePredictor = null;
+        }
+        isLoaded = false;
+        cpuThreadNum = 1;
+        cpuPowerMode = "LITE_POWER_HIGH";
+        modelPath = "";
+        modelName = "";
+    }
+
+    protected boolean loadLabel(Context appCtx, String labelPath) {
+        wordLabels.clear();
+        // Load word labels from file
+        try {
+            InputStream assetsInputStream = appCtx.getAssets().open(labelPath);
+            int available = assetsInputStream.available();
+            byte[] lines = new byte[available];
+            assetsInputStream.read(lines);
+            assetsInputStream.close();
+            String words = new String(lines);
+            String[] contents = words.split("\n");
+            for (String content : contents) {
+                wordLabels.add(content);
+            }
+            Log.i(TAG, "Word label size: " + wordLabels.size());
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean runModel() {
+        if (inputImage == null || !isLoaded()) {
+            return false;
+        }
+
+        // Pre-process image, and feed input tensor with pre-processed data
+
+        Bitmap scaleImage = Utils.resizeWithStep(inputImage, Long.valueOf(inputShape[2]).intValue(), 32);
+
+        Date start = new Date();
+        int channels = (int) inputShape[1];
+        int width = scaleImage.getWidth();
+        int height = scaleImage.getHeight();
+        float[] inputData = new float[channels * width * height];
+        if (channels == 3) {
+            int[] channelIdx = null;
+            if (inputColorFormat.equalsIgnoreCase("RGB")) {
+                channelIdx = new int[]{0, 1, 2};
+            } else if (inputColorFormat.equalsIgnoreCase("BGR")) {
+                channelIdx = new int[]{2, 1, 0};
+            } else {
+                Log.i(TAG, "Unknown color format " + inputColorFormat + ", only RGB and BGR color format is " +
+                        "supported!");
+                return false;
+            }
+            int[] channelStride = new int[]{width * height, width * height * 2};
+            int p = scaleImage.getPixel(scaleImage.getWidth() - 1, scaleImage.getHeight() - 1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int color = scaleImage.getPixel(x, y);
+                    float[] rgb = new float[]{(float) red(color) / 255.0f, (float) green(color) / 255.0f,
+                            (float) blue(color) / 255.0f};
+                    inputData[y * width + x] = (rgb[channelIdx[0]] - inputMean[0]) / inputStd[0];
+                    inputData[y * width + x + channelStride[0]] = (rgb[channelIdx[1]] - inputMean[1]) / inputStd[1];
+                    inputData[y * width + x + channelStride[1]] = (rgb[channelIdx[2]] - inputMean[2]) / inputStd[2];
+
+                }
+            }
+        } else if (channels == 1) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int color = inputImage.getPixel(x, y);
+                    float gray = (float) (red(color) + green(color) + blue(color)) / 3.0f / 255.0f;
+                    inputData[y * width + x] = (gray - inputMean[0]) / inputStd[0];
+                }
+            }
+        } else {
+            Log.i(TAG, "Unsupported channel size " + Integer.toString(channels) + ",  only channel 1 and 3 is " +
+                    "supported!");
+            return false;
+        }
+        float[] pixels = inputData;
+        Log.i(TAG, "pixels " + pixels[0] + " " + pixels[1] + " " + pixels[2] + " " + pixels[3]
+                + " " + pixels[pixels.length / 2] + " " + pixels[pixels.length / 2 + 1] + " " + pixels[pixels.length - 2] + " " + pixels[pixels.length - 1]);
+        Date end = new Date();
+        preprocessTime = (float) (end.getTime() - start.getTime());
+
+        // Warm up
+        for (int i = 0; i < warmupIterNum; i++) {
+            paddlePredictor.runImage(inputData, width, height, channels, inputImage);
+        }
+        warmupIterNum = 0; // do not need warm
+        // Run inference
+        start = new Date();
+        ArrayList<OcrResultModel> results = paddlePredictor.runImage(inputData, width, height, channels, inputImage);
+        end = new Date();
+        inferenceTime = (end.getTime() - start.getTime()) / (float) inferIterNum;
+
+        results = postprocess(results);
+        Log.i(TAG, "[stat] Preprocess Time: " + preprocessTime
+                + " ; Inference Time: " + inferenceTime + " ;Box Size " + results.size());
+        drawResults(results);
+
+        return true;
+    }
+
+
+    public boolean isLoaded() {
+        return paddlePredictor != null && isLoaded;
+    }
+
+    public String modelPath() {
+        return modelPath;
+    }
+
+    public String modelName() {
+        return modelName;
+    }
+
+    public int cpuThreadNum() {
+        return cpuThreadNum;
+    }
+
+    public String cpuPowerMode() {
+        return cpuPowerMode;
+    }
+
+    public float inferenceTime() {
+        return inferenceTime;
+    }
+
+    public Bitmap inputImage() {
+        return inputImage;
+    }
+
+    public Bitmap outputImage() {
+        return outputImage;
+    }
+
+    public String outputResult() {
+        return outputResult;
+    }
+
+    public float preprocessTime() {
+        return preprocessTime;
+    }
+
+    public float postprocessTime() {
+        return postprocessTime;
+    }
+
+
+    public void setInputImage(Bitmap image) {
+        if (image == null) {
+            return;
+        }
+        this.inputImage = image.copy(Bitmap.Config.ARGB_8888, true);
+    }
+
+    private ArrayList<OcrResultModel> postprocess(ArrayList<OcrResultModel> results) {
+        for (OcrResultModel r : results) {
+            StringBuffer word = new StringBuffer();
+            for (int index : r.getWordIndex()) {
+                if (index >= 0 && index < wordLabels.size()) {
+                    word.append(wordLabels.get(index));
+                } else {
+                    Log.e(TAG, "Word index is not in label list:" + index);
+                    word.append("×");
+                }
+            }
+            r.setLabel(word.toString());
+        }
+        return results;
+    }
+
+    private void drawResults(ArrayList<OcrResultModel> results) {
+        StringBuffer outputResultSb = new StringBuffer("");
+        for (int i = 0; i < results.size(); i++) {
+            OcrResultModel result = results.get(i);
+            StringBuilder sb = new StringBuilder("");
+            sb.append(result.getLabel());
+            sb.append(" ").append(result.getConfidence());
+            sb.append("; Points: ");
+            for (Point p : result.getPoints()) {
+                sb.append("(").append(p.x).append(",").append(p.y).append(") ");
+            }
+            Log.i(TAG, sb.toString()); // show LOG in Logcat panel
+            outputResultSb.append(i + 1).append(": ").append(result.getLabel()).append("\n");
+        }
+        outputResult = outputResultSb.toString();
+        outputImage = inputImage;
+        Canvas canvas = new Canvas(outputImage);
+        Paint paintFillAlpha = new Paint();
+        paintFillAlpha.setStyle(Paint.Style.FILL);
+        paintFillAlpha.setColor(Color.parseColor("#3B85F5"));
+        paintFillAlpha.setAlpha(50);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#3B85F5"));
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
+
+        for (OcrResultModel result : results) {
+            Path path = new Path();
+            List<Point> points = result.getPoints();
+            path.moveTo(points.get(0).x, points.get(0).y);
+            for (int i = points.size() - 1; i >= 0; i--) {
+                Point p = points.get(i);
+                path.lineTo(p.x, p.y);
+            }
+            canvas.drawPath(path, paint);
+            canvas.drawPath(path, paintFillAlpha);
+        }
+    }
+
+}
