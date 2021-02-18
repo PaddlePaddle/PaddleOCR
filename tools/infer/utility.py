5fc33c1213f6e3b40da030bb7d8ae73110649eb3
LDOUBLEV <liuvv0203@outlook.com>
2021-02-18 18:45:53 +0800

    keep dilation as False

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -47,6 +47,7 @@ def parse_args():
     parser.add_argument("--det_db_box_thresh", type=float, default=0.5)
     parser.add_argument("--det_db_unclip_ratio", type=float, default=1.6)
     parser.add_argument("--max_batch_size", type=int, default=10)
+    parser.add_argument("--use_dilation", type=bool, default=False)
     # EAST parmas
     parser.add_argument("--det_east_score_thresh", type=float, default=0.8)
     parser.add_argument("--det_east_cover_thresh", type=float, default=0.1)
