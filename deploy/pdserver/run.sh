b7186e2cb5f24c70844c2fe211599b6ceffb3b73
LDOUBLEV <liuvv0203@outlook.com>
2021-03-05 15:46:01 +0800

    add pdserver demo

---
+++ deploy/pdserver/run.sh
@@ -0,0 +1,10 @@
+python3.7 pipeline_http_client.py &
+python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
+# python3.7 pipeline_http_client.py &
