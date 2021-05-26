b4dc3e9876abeab2ca307bb3f5828acd9638856a
LDOUBLEV <liuvv0203@outlook.com>
2021-05-26 08:04:12 +0000

    complie path retrieval

--- deploy/cpp_infer/tools/build.sh
+++ deploy/cpp_infer/tools/build.sh
@@ -1,8 +1,7 @@
-OPENCV_DIR=/paddle/Paddle/opencv-3.4.7/opencv3
-LIB_DIR=/paddle/OCR/debug/paddle_inference
-CUDA_LIB_DIR=/usr/local/cuda/lib64
-CUDNN_LIB_DIR=/usr/lib/x86_64-linux-gnu
-TENSORRT_DIR=/paddle/Paddle/package/TensorRT/TensorRT-6.0.1.5/
+OPENCV_DIR=your_opencv_dir
+LIB_DIR=your_paddle_inference_dir
+CUDA_LIB_DIR=your_cuda_lib_dir
+CUDNN_LIB_DIR=your_cudnn_lib_dir
 
 BUILD_DIR=build
 rm -rf ${BUILD_DIR}
