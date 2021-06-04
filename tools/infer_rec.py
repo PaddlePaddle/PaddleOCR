115955f7221d0d331a5a0d27bd0cd4628921d2a1
littletomatodonkey <dazhiningsibuqu@163.com>
2021-06-04 02:46:45 +0000

    fix metric

--- tools/infer_rec.py
+++ tools/infer_rec.py
@@ -46,8 +46,14 @@ def main():
 
     # build model
     if hasattr(post_process_class, 'character'):
-        config['Architecture']["Head"]['out_channels'] = len(
-            getattr(post_process_class, 'character'))
+        char_num = len(getattr(post_process_class, 'character'))
+        if config['Architecture']["algorithm"] in ["Distillation",
+                                                   ]:  # distillation model
+            for key in config['Architecture']["Models"]:
+                config['Architecture']["Models"][key]["Head"][
+                    'out_channels'] = char_num
+        else:  # base rec model
+            config['Architecture']["Head"]['out_channels'] = char_num
 
     model = build_model(config['Architecture'])
 
