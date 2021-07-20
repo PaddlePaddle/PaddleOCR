9480f212ce5c76e3168c35e1bb6ec991a51ce6db
Wei-JL <71386288+Wei-JL@users.noreply.github.com>
2021-07-20 13:05:30 +0800

    修正更新资源文件

--- PPOCRLabel/libs/resources.py
+++ PPOCRLabel/libs/resources.py
@@ -5,22 +5,3 @@
 # Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
 #
 # WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore
-
-
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
-if qt_version < [5, 8, 0]:
-    rcc_version = 1
-    qt_resource_struct = qt_resource_struct_v1
-else:
-    rcc_version = 2
-    qt_resource_struct = qt_resource_struct_v2
-
-def qInitResources():
-    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
-
-def qCleanupResources():
-    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
-
-qInitResources()
