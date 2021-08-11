b5a86e67319ca77ab15f823c6d4c6158e2300c04
MissPenguin <lichenxia1991@163.com>
2021-08-11 13:12:51 +0000

    revert

--- deploy/cpp_infer/tools/build.sh
+++ deploy/cpp_infer/tools/build.sh
@@ -1,7 +1,7 @@
-OPENCV_DIR=/paddle/git/new/PaddleOCR/deploy/cpp_infer/opencv-3.4.7/opencv3/
-LIB_DIR=/paddle/git/new/PaddleOCR/deploy/cpp_infer/paddle_inference/
-CUDA_LIB_DIR=/usr/local/cuda/lib64/
-CUDNN_LIB_DIR=/usr/lib/x86_64-linux-gnu/
+OPENCV_DIR=your_opencv_dir
+LIB_DIR=your_paddle_inference_dir
+CUDA_LIB_DIR=your_cuda_lib_dir
+CUDNN_LIB_DIR=your_cudnn_lib_dir
 
 
 BUILD_DIR=build
