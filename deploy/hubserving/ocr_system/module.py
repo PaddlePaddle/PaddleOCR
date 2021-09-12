9d3f186777941a71421c0a3fdd39188592f88352
tianfupang <397720544@qq.com>
2021-09-13 00:39:05 +0800

    Fix TextSystem dumps cfg log

--- deploy/hubserving/ocr_system/module.py
+++ deploy/hubserving/ocr_system/module.py
@@ -5,17 +5,18 @@ from __future__ import print_function
 
 import os
 import sys
+
 sys.path.insert(0, ".")
 import copy
 
 import time
 
 from paddlehub.common.logger import logger
-from paddlehub.module.module import moduleinfo, runnable, serving
+from paddlehub.module.module import moduleinfo, serving
 import cv2
 import numpy as np
 import paddlehub as hub
-
+import json
 from tools.infer.utility import base64_to_cv2
 from tools.infer.predict_system import TextSystem
 from tools.infer.utility import parse_args
@@ -50,7 +51,7 @@ class OCRSystem(hub.Module):
                 )
         cfg.ir_optim = True
         cfg.enable_mkldnn = enable_mkldnn
-
+        logger.info("TextSystem dumps cfg: {}", json.dumps(cfg, sort_keys=True, indent=4, separators=(', ', ': ')))
         self.text_sys = TextSystem(cfg)
 
     def merge_configs(self, ):
