ecb49e8df3946f16b95e78da28eb5673da2fd9b9
littletomatodonkey <dazhiningsibuqu@163.com>
2021-10-12 16:20:37 +0800

    rm unused import

--- ppocr/modeling/backbones/rec_mv1_enhance.py
+++ ppocr/modeling/backbones/rec_mv1_enhance.py
@@ -16,26 +16,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
 import numpy as np
 import paddle
-from paddle import ParamAttr
+from paddle import ParamAttr, reshape, transpose
 import paddle.nn as nn
 import paddle.nn.functional as F
 from paddle.nn import Conv2D, BatchNorm, Linear, Dropout
 from paddle.nn import AdaptiveAvgPool2D, MaxPool2D, AvgPool2D
 from paddle.nn.initializer import KaimingNormal
-import math
-import numpy as np
-import paddle
-from paddle import ParamAttr, reshape, transpose, concat, split
-import paddle.nn as nn
-import paddle.nn.functional as F
-from paddle.nn import Conv2D, BatchNorm, Linear, Dropout
-from paddle.nn import AdaptiveAvgPool2D, MaxPool2D, AvgPool2D
-from paddle.nn.initializer import KaimingNormal
-import math
-from paddle.nn.functional import hardswish, hardsigmoid
 from paddle.regularizer import L2Decay
+from paddle.nn.functional import hardswish, hardsigmoid
 
 
 class ConvBNLayer(nn.Layer):
