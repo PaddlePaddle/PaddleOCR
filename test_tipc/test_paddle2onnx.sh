f761ca4bdea890255d9817e5dc0cff133ef624d6
tink2123 <y_tink@163.com>
2021-11-02 15:49:54 +0800

    add paddle2onnx doc

--- test_tipc/test_paddle2onnx.sh
+++ test_tipc/test_paddle2onnx.sh
@@ -38,7 +38,7 @@ image_dir_key=$(func_parser_key "${lines[11]}")
 image_dir_value=$(func_parser_value "${lines[11]}")
 
 
-LOG_PATH="../../test_tipc/output"
+LOG_PATH="./test_tipc/output"
 mkdir -p ./test_tipc/output
 status_log="${LOG_PATH}/results_paddle2onnx.log"
 
@@ -48,6 +48,7 @@ function func_paddle2onnx(){
     _script=$1
 
     # paddle2onnx
+    _save_log_path="${LOG_PATH}/paddle2onnx_infer_cpu.log"
     set_dirname=$(func_set_params "${infer_model_dir_key}" "${infer_model_dir_value}")
     set_model_filename=$(func_set_params "${model_filename_key}" "${model_filename_value}")
     set_params_filename=$(func_set_params "${params_filename_key}" "${params_filename_value}")
@@ -56,12 +57,15 @@ function func_paddle2onnx(){
     set_enable_onnx_checker=$(func_set_params "${enable_onnx_checker_key}" "${enable_onnx_checker_value}")
     trans_model_cmd="${padlle2onnx_cmd} ${set_dirname} ${set_model_filename} ${set_params_filename} ${set_save_model} ${set_opset_version} ${set_enable_onnx_checker}"
     eval $trans_model_cmd
+    last_status=${PIPESTATUS[0]}
+    status_check $last_status "${trans_model_cmd}" "${status_log}"
     # python inference
     set_gpu=$(func_set_params "${use_gpu_key}" "${use_gpu_value}")
     set_model_dir=$(func_set_params "${det_model_key}" "${save_file_value}")
     set_img_dir=$(func_set_params "${image_dir_key}" "${image_dir_value}")
-    infer_model_cmd="${python} ${inference_py} ${set_gpu} ${set_img_dir} ${set_model_dir}"
+    infer_model_cmd="${python} ${inference_py} ${set_gpu} ${set_img_dir} ${set_model_dir} > ${_save_log_path} 2>&1 "
     eval $infer_model_cmd
+    status_check $last_status "${infer_model_cmd}" "${status_log}"
 }
 
 
