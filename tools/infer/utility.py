4cb2179b7377587ef0d6b67f65de4b70e893dbdb
LDOUBLEV <liuvv0203@outlook.com>
2021-11-08 19:35:15 +0800

    set reasonable trt shape

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -193,7 +193,7 @@ def create_predictor(args, mode, logger):
                 "nearest_interp_v2_0.tmp_0": [1, 256, 2, 2]
             }
             max_input_shape = {
-                "x": [1, 3, 2000, 2000],
+                "x": [1, 3, 1280, 1280],
                 "conv2d_92.tmp_0": [1, 120, 400, 400],
                 "conv2d_91.tmp_0": [1, 24, 200, 200],
                 "conv2d_59.tmp_0": [1, 96, 400, 400],
@@ -243,16 +243,16 @@ def create_predictor(args, mode, logger):
             opt_input_shape.update(opt_pact_shape)
         elif mode == "rec":
             min_input_shape = {"x": [1, 3, 32, 10]}
-            max_input_shape = {"x": [args.rec_batch_num, 3, 32, 2000]}
+            max_input_shape = {"x": [args.rec_batch_num, 3, 32, 1024]}
             opt_input_shape = {"x": [args.rec_batch_num, 3, 32, 320]}
         elif mode == "cls":
             min_input_shape = {"x": [1, 3, 48, 10]}
-            max_input_shape = {"x": [args.rec_batch_num, 3, 48, 2000]}
+            max_input_shape = {"x": [args.rec_batch_num, 3, 48, 1024]}
             opt_input_shape = {"x": [args.rec_batch_num, 3, 48, 320]}
         else:
             min_input_shape = {"x": [1, 3, 10, 10]}
-            max_input_shape = {"x": [1, 3, 1000, 1000]}
-            opt_input_shape = {"x": [1, 3, 500, 500]}
+            max_input_shape = {"x": [1, 3, 512, 512]}
+            opt_input_shape = {"x": [1, 3, 256, 256]}
         config.set_trt_dynamic_shape_info(min_input_shape, max_input_shape,
                                           opt_input_shape)
 
