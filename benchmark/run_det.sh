8a6bc665cce0bfd68cfd4349fbd257b8b71942fe
hysunflower <nothing_upc@163.com>
2021-11-30 06:59:47 +0000

    fix_log_name

--- benchmark/run_det.sh
+++ benchmark/run_det.sh
@@ -2,6 +2,7 @@
 # 提供可稳定复现性能的脚本，默认在标准docker环境内py37执行： paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7  paddle=2.1.2  py=37
 # 执行目录: ./PaddleOCR
 # 1 安装该模型需要的依赖 (如需开启优化策略请注明)
+log_path=${LOG_PATH_INDEX_DIR:-$(pwd)}
 python -m pip install -r requirements.txt
 # 2 拷贝该模型需要数据、预训练模型
 wget -P ./train_data/  https://paddleocr.bj.bcebos.com/dygraph_v2.0/test/icdar2015.tar && cd train_data  && tar xf icdar2015.tar && cd ../
@@ -18,12 +19,12 @@ for model_mode in ${model_mode_list[@]}; do
           for bs_item in ${bs_list[@]}; do
             echo "index is speed, 1gpus, begin, ${model_name}"
             run_mode=sp
-            log_name=ocr_${model_mode}_${run_mode}_bs${bs_item}_${fp_item}
+            log_name=ocr_${model_mode}_bs${bs_item}_${fp_item}
             CUDA_VISIBLE_DEVICES=0 bash benchmark/run_benchmark_det.sh ${run_mode} ${bs_item} ${fp_item} 1 ${model_mode} | tee ${log_path}/${log_name}_speed_1gpus 2>&1    #  (5min)
             sleep 60
             echo "index is speed, 8gpus, run_mode is multi_process, begin, ${model_name}"
             run_mode=mp
-            log_name=ocr_${model_mode}_${run_mode}_bs${bs_item}_${fp_item}
+            log_name=ocr_${model_mode}_bs${bs_item}_${fp_item}
             CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash benchmark/run_benchmark_det.sh ${run_mode} ${bs_item} ${fp_item} 2 ${model_mode} | tee ${log_path}/${log_name}_speed_8gpus8p 2>&1
             sleep 60
             done
