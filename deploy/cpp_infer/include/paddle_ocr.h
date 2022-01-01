7cdf2f60891488c3b86874eb42c0b7deacbe3bd9
MistEO <mistereo@hotmail.com>
2022-01-01 23:40:18 +0800

    add interface with image data

--- deploy/cpp_infer/include/paddle_ocr.h
+++ deploy/cpp_infer/include/paddle_ocr.h
@@ -13,26 +13,43 @@ typedef unsigned char uint8_t;
 extern "C" {
 #endif
 
-OCRAPI_PORT paddle_ocr_t* OCR_CALL PaddleOcrCreate(
-    const char* det_model_dir, const char* rec_model_dir,
-    const char* char_list_file, const char* cls_model_dir);
+    OCRAPI_PORT paddle_ocr_t* OCR_CALL PaddleOcrCreate(
+        const char* det_model_dir, const char* rec_model_dir,
+        const char* char_list_file, const char* cls_model_dir);
 
-void OCRAPI PaddleOcrDestroy(paddle_ocr_t* ocr_ptr);
+    void OCRAPI PaddleOcrDestroy(paddle_ocr_t* ocr_ptr);
 
-OCR_ERROR OCRAPI PaddleOcrDet(
-    paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
-    int* out_boxes, size_t* out_boxes_size,
-    double* out_times, size_t* out_times_size);
+    OCR_ERROR OCRAPI PaddleOcrDet(
+        paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
+        int* out_boxes, size_t* out_boxes_size,
+        double* out_times, size_t* out_times_size);
 
-OCR_ERROR OCRAPI PaddleOcrRec(
-    paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
-    char** out_strs, float* out_scores, size_t* out_size,
-    double* out_times, size_t* out_times_size);
+    OCR_ERROR OCRAPI PaddleOcrDetWithData(
+        paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+        int* out_boxes, size_t* out_boxes_size,
+        double* out_times, size_t* out_times_size);
 
-OCR_ERROR OCRAPI PaddleOcrSystem(
-    paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size, bool with_cls,
-    int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
-    double* out_times, size_t* out_times_size);
+    OCR_ERROR OCRAPI PaddleOcrRec(
+        paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
+        char** out_strs, float* out_scores, size_t* out_size,
+        double* out_times, size_t* out_times_size);
+
+    OCR_ERROR OCRAPI PaddleOcrRecWithData(
+        paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+        char** out_strs, float* out_scores, size_t* out_size,
+        double* out_times, size_t* out_times_size);
+
+    OCR_ERROR OCRAPI PaddleOcrSystem(
+        paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
+        bool with_cls,
+        int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+        double* out_times, size_t* out_times_size);
+
+    OCR_ERROR OCRAPI PaddleOcrSystemWithData(
+        paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+        bool with_cls,
+        int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+        double* out_times, size_t* out_times_size);
 
 #ifdef __cplusplus
 }
--- deploy/cpp_infer/src/paddle_ocr.cpp
+++ deploy/cpp_infer/src/paddle_ocr.cpp
@@ -38,7 +38,8 @@ void vector2pointer(const std::vector<T>& vec, T* out, size_t* out_size)
         if (out_size) {
             *out_size = vec.size();
         }
-    } else if (out_size) {
+    }
+    else if (out_size) {
         *out_size = 0;
     }
 }
@@ -87,6 +88,30 @@ void PaddleOcrDestroy(paddle_ocr_t* ocr_ptr)
     ocr_ptr = nullptr;
 }
 
+OCR_ERROR _PaddleOcrDet(
+    paddle_ocr_t* ocr_ptr, cv::Mat& srcimg,
+    int* out_boxes, size_t* out_boxes_size,
+    double* out_times, size_t* out_times_size)
+{
+    std::vector<std::vector<std::vector<int>>> boxes;
+    std::vector<double> det_times;
+
+    ocr_ptr->det_ptr->Run(srcimg, boxes, &det_times);
+
+    *out_boxes_size = boxes.size();
+    // each box has 8 values
+    for (size_t i = 0; i != boxes.size(); ++i) {
+        for (size_t j = 0; j != 4; ++j) {
+            *(out_boxes + i * 8 + j * 2) = boxes[i][j][0];
+            *(out_boxes + i * 8 + j * 2 + 1) = boxes[i][j][1];
+        }
+    }
+
+    vector2pointer(det_times, out_times, out_times_size);
+
+    return OCR_SUCCESS;
+}
+
 OCR_ERROR PaddleOcrDet(
     paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
     int* out_boxes, size_t* out_boxes_size,
@@ -104,21 +129,47 @@ OCR_ERROR PaddleOcrDet(
         return OCR_FAILURE;
     }
 
-    std::vector<std::vector<std::vector<int>>> boxes;
-    std::vector<double> det_times;
+    return _PaddleOcrDet(ocr_ptr, srcimg,
+        out_boxes, out_boxes_size,
+        out_times, out_times_size);
+}
 
-    ocr_ptr->det_ptr->Run(srcimg, boxes, &det_times);
-
-    *out_boxes_size = boxes.size();
-    // each box has 8 values
-    for (size_t i = 0; i != boxes.size(); ++i) {
-        for (size_t j = 0; j != 4; ++j) {
-            *(out_boxes + i * 8 + j * 2) = boxes[i][j][0];
-            *(out_boxes + i * 8 + j * 2 + 1) = boxes[i][j][1];
-        }
+OCR_ERROR PaddleOcrDetWithData(
+    paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+    int* out_boxes, size_t* out_boxes_size,
+    double* out_times, size_t* out_times_size)
+{
+    if (ocr_ptr == nullptr
+        || data == nullptr
+        || out_boxes == nullptr
+        || out_boxes_size == nullptr) {
+        return OCR_FAILURE;
     }
 
-    vector2pointer(det_times, out_times, out_times_size);
+    cv::Mat srcimg(rows, cols, type, data);
+    if (srcimg.empty()) {
+        return OCR_FAILURE;
+    }
+
+    return _PaddleOcrDet(ocr_ptr, srcimg,
+        out_boxes, out_boxes_size,
+        out_times, out_times_size);
+}
+
+OCR_ERROR _PaddleOcrRec(
+    paddle_ocr_t* ocr_ptr, cv::Mat& srcimg,
+    char** out_strs, float* out_scores, size_t* out_size,
+    double* out_times, size_t* out_times_size)
+{
+    std::vector<string> strs_res;
+    std::vector<float> scores;
+    std::vector<double> rec_times;
+
+    ocr_ptr->rec_ptr->Run(srcimg, strs_res, scores, &rec_times);
+
+    stringvec_to_charpp(strs_res, out_strs, out_size);
+    memcpy(out_scores, scores.data(), scores.size() * sizeof(float));
+    vector2pointer(rec_times, out_times, out_times_size);
 
     return OCR_SUCCESS;
 }
@@ -141,38 +192,40 @@ OCR_ERROR PaddleOcrRec(
         return OCR_FAILURE;
     }
 
-    std::vector<string> strs_res;
-    std::vector<float> scores;
-    std::vector<double> rec_times;
-
-    ocr_ptr->rec_ptr->Run(srcimg, strs_res, scores, &rec_times);
-
-    stringvec_to_charpp(strs_res, out_strs, out_size);
-    memcpy(out_scores, scores.data(), scores.size() * sizeof(float));
-    vector2pointer(rec_times, out_times, out_times_size);
-
-    return OCR_SUCCESS;
+    return _PaddleOcrRec(ocr_ptr, srcimg,
+        out_strs, out_scores, out_size,
+        out_times, out_times_size);
 }
 
-OCR_ERROR PaddleOcrSystem(
-    paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size, bool with_cls,
-    int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+OCR_ERROR PaddleOcrRecWithData(
+    paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+    char** out_strs, float* out_scores, size_t* out_size,
     double* out_times, size_t* out_times_size)
 {
     if (ocr_ptr == nullptr
-        || encode_buf == nullptr
-        || out_boxes == nullptr
+        || data == nullptr
         || out_strs == nullptr
         || out_scores == nullptr
         || out_size == nullptr) {
         return OCR_FAILURE;
     }
 
-    cv::Mat srcimg = decode(encode_buf, encode_buf_size);
+    cv::Mat srcimg(rows, cols, type, data);
     if (srcimg.empty()) {
         return OCR_FAILURE;
     }
 
+    return _PaddleOcrRec(ocr_ptr, srcimg,
+        out_strs, out_scores, out_size,
+        out_times, out_times_size);
+}
+
+OCR_ERROR _PaddleOcrSystem(
+    paddle_ocr_t* ocr_ptr, cv::Mat& srcimg,
+    bool with_cls,
+    int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+    double* out_times, size_t* out_times_size)
+{
     std::vector<std::vector<std::vector<int>>> boxes;
     std::vector<double> all_times;
 
@@ -204,6 +257,56 @@ OCR_ERROR PaddleOcrSystem(
     vector2pointer(all_times, out_times, out_times_size);
 
     return OCR_SUCCESS;
+}
+
+OCR_ERROR PaddleOcrSystem(
+    paddle_ocr_t* ocr_ptr, const uint8_t* encode_buf, size_t encode_buf_size,
+    bool with_cls,
+    int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+    double* out_times, size_t* out_times_size)
+{
+    if (ocr_ptr == nullptr
+        || encode_buf == nullptr
+        || out_boxes == nullptr
+        || out_strs == nullptr
+        || out_scores == nullptr
+        || out_size == nullptr) {
+        return OCR_FAILURE;
+    }
+
+    cv::Mat srcimg = decode(encode_buf, encode_buf_size);
+    if (srcimg.empty()) {
+        return OCR_FAILURE;
+    }
+
+    return _PaddleOcrSystem(ocr_ptr, srcimg, with_cls,
+        out_boxes, out_strs, out_scores, out_size,
+        out_times, out_times_size);
+}
+
+OCR_ERROR OCRAPI PaddleOcrSystemWithData(
+    paddle_ocr_t* ocr_ptr, int rows, int cols, int type, void* data,
+    bool with_cls,
+    int* out_boxes, char** out_strs, float* out_scores, size_t* out_size,
+    double* out_times, size_t* out_times_size)
+{
+    if (ocr_ptr == nullptr
+        || data == nullptr
+        || out_boxes == nullptr
+        || out_strs == nullptr
+        || out_scores == nullptr
+        || out_size == nullptr) {
+        return OCR_FAILURE;
+    }
+
+    cv::Mat srcimg(rows, cols, type, data);
+    if (srcimg.empty()) {
+        return OCR_FAILURE;
+    }
+
+    return _PaddleOcrSystem(ocr_ptr, srcimg, with_cls,
+        out_boxes, out_strs, out_scores, out_size,
+        out_times, out_times_size);
 }
 
 #endif // OCR_EXPORTS
