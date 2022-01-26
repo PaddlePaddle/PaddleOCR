97aa6834d2c63b046f295db736beb7599cf4c932
halleywj <wangjunzju@zju.edu.cn>
2022-01-26 11:22:28 +0800

    对 pdserving pipeline_http_client.py 增加多线程测速功能

--- deploy/pdserving/pipeline_http_client.py
+++ deploy/pdserving/pipeline_http_client.py
@@ -17,29 +17,38 @@ import requests
 import json
 import base64
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import argparse
 parser = argparse.ArgumentParser(description="args for paddleserving")
 parser.add_argument("--image_dir", type=str, default="../../doc/imgs/")
+parser.add_argument('--num_thread', type=int, default=35)
 args = parser.parse_args()
 
 
 def cv2_to_base64(image):
     return base64.b64encode(image).decode('utf8')
 
-
-url = "http://127.0.0.1:9998/ocr/prediction"
-test_img_dir = args.image_dir
-
-for idx, img_file in enumerate(os.listdir(test_img_dir)):
-    with open(os.path.join(test_img_dir, img_file), 'rb') as file:
+def handle(filename):
+    with open(filename, 'rb') as file:
         image_data1 = file.read()
 
     image = cv2_to_base64(image_data1)
 
-    for i in range(1):
-        data = {"key": ["image"], "value": [image]}
-        r = requests.post(url=url, data=json.dumps(data))
-        print(r.json())
+    data = {"key": ["image"], "value": [image]}
+    r = requests.post(url=url, data=json.dumps(data))
+    return r.json()
+
+
+url = "http://127.0.0.1:9998/ocr/prediction"
+test_img_dir = args.image_dir
+
+start = time.time()
+with ThreadPoolExecutor(max_workers=args.num_thread) as executor:
+    futures = [executor.submit(handle, os.path.join(test_img_dir, img_file)) for idx, img_file in enumerate(os.listdir(test_img_dir))]
+    for future in as_completed(futures):
+        print(future.result())
 
 print("==> total number of test imgs: ", len(os.listdir(test_img_dir)))
+print('==> total time cost:', time.time() - start)
