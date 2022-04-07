22b57e6e85739ea2bf875cd2a9a348a7e3d7ec63
LDOUBLEV <liuvv0203@outlook.com>
2022-04-07 08:26:20 +0000

    fix rec pact hang

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -64,6 +64,10 @@ if [ ${MODE} = "lite_train_lite_infer" ];then
         wget  -nc -P  ./pretrain_models/  https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_det_train.tar --no-check-certificate
         cd ./pretrain_models/ && tar xf ch_ppocr_server_v2.0_det_train.tar  && cd ../
     fi
+    if [ ${model_name} == "ch_PPOCRv2_rec" ] || [ ${model_name} == "ch_PPOCRv2_rec_PACT" ]; then
+        wget  -nc -P  ./pretrain_models/  https://paddleocr.bj.bcebos.com/PP-OCRv2/chinese/ch_PP-OCRv2_rec_train.tar --no-check-certificate
+        cd ./pretrain_models/ && tar xf ch_PP-OCRv2_rec_train.tar && cd ../
+    fi
     if [ ${model_name} == "det_r18_db_v2_0" ]; then
         wget -nc -P ./pretrain_models/  https://paddleocr.bj.bcebos.com/pretrained/ResNet18_vd_pretrained.pdparams  --no-check-certificate
     fi
@@ -90,6 +94,10 @@ if [ ${MODE} = "lite_train_lite_infer" ];then
     if [ ${model_name} == "ch_ppocr_mobile_v2.0_rec_FPGM" ]; then
         wget -nc -P ./pretrain_models/ https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_train.tar --no-check-certificate
         cd ./pretrain_models/ && tar xf ch_ppocr_mobile_v2.0_rec_train.tar && cd ../
+    fi
+    if [ ${model_name} == "det_mv3_east_v2.0" ]; then
+        wget -nc -P ./pretrain_models/ https://paddleocr.bj.bcebos.com/dygraph_v2.0/en/det_mv3_east_v2.0_train.tar --no-check-certificate
+        cd ./pretrain_models/ && tar xf det_mv3_east_v2.0_train.tar && cd ../
     fi
 
 elif [ ${MODE} = "whole_train_whole_infer" ];then
