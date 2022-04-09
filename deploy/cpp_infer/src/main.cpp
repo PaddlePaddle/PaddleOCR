f889907c1c06e9c70b81d11a997bae1ef4dbb57c
WenmuZhou <572459439@qq.com>
2022-04-09 03:42:36 +0000

    move args to args.h

--- deploy/cpp_infer/src/main.cpp
+++ deploy/cpp_infer/src/main.cpp
@@ -27,6 +27,7 @@
 #include <fstream>
 #include <numeric>
 
+#include <include/args.h>
 #include <include/ocr_cls.h>
 #include <include/ocr_det.h>
 #include <include/ocr_rec.h>
@@ -34,37 +35,6 @@
 #include <sys/stat.h>
 
 #include "auto_log/autolog.h"
-#include <gflags/gflags.h>
-
-// common args
-DEFINE_bool(use_gpu, false, "Infering with GPU or CPU.");
-DEFINE_bool(use_tensorrt, false, "Whether use tensorrt.");
-DEFINE_int32(gpu_id, 0, "Device id of GPU to execute.");
-DEFINE_int32(gpu_mem, 4000, "GPU id when infering with GPU.");
-DEFINE_int32(cpu_threads, 10, "Num of threads with CPU.");
-DEFINE_bool(enable_mkldnn, false, "Whether use mkldnn with CPU.");
-DEFINE_string(precision, "fp32", "Precision be one of fp32/fp16/int8");
-DEFINE_bool(benchmark, false, "Whether use benchmark.");
-DEFINE_string(output, "./output/", "Save benchmark log path.");
-DEFINE_string(image_dir, "", "Dir of input image.");
-DEFINE_bool(visualize, true, "Whether show the detection results.");
-// detection related
-DEFINE_string(det_model_dir, "", "Path of det inference model.");
-DEFINE_int32(max_side_len, 960, "max_side_len of input image.");
-DEFINE_double(det_db_thresh, 0.3, "Threshold of det_db_thresh.");
-DEFINE_double(det_db_box_thresh, 0.6, "Threshold of det_db_box_thresh.");
-DEFINE_double(det_db_unclip_ratio, 1.5, "Threshold of det_db_unclip_ratio.");
-DEFINE_bool(use_dilation, false, "Whether use the dilation on output map.");
-DEFINE_string(det_db_score_mode, "slow", "Whether use polygon score.");
-// classification related
-DEFINE_bool(use_angle_cls, false, "Whether use use_angle_cls.");
-DEFINE_string(cls_model_dir, "", "Path of cls inference model.");
-DEFINE_double(cls_thresh, 0.9, "Threshold of cls_thresh.");
-// recognition related
-DEFINE_string(rec_model_dir, "", "Path of rec inference model.");
-DEFINE_int32(rec_batch_num, 6, "rec_batch_num.");
-DEFINE_string(rec_char_dict_path, "../../ppocr/utils/ppocr_keys_v1.txt",
-              "Path of dictionary.");
 
 using namespace std;
 using namespace cv;
