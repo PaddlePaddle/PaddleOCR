b62e6954adfab0edb4a3fcd9835f147c89111806
LDOUBLEV <liuvv0203@outlook.com>
2022-04-12 14:00:07 +0800

    fix rec output name error

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -323,6 +323,10 @@ def get_output_tensors(args, mode, predictor):
         output_name = 'softmax_0.tmp_0'
         if output_name in output_names:
             return [predictor.get_output_handle(output_name)]
+        else:
+            for output_name in output_names:
+                output_tensor = predictor.get_output_handle(output_name)
+                output_tensors.append(output_tensor)
     else:
         for output_name in output_names:
             output_tensor = predictor.get_output_handle(output_name)
