4ec14d0e8f5539f42b3c7b527e6a5792752a23d1
Liliya Wu <94680624+Liliyaw@users.noreply.github.com>
2022-07-04 21:33:38 +0800

    Update run_demo.sh

--- deploy/avh/run_demo.sh
+++ deploy/avh/run_demo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Copyright (c) 2022 Arm Limited and Contributors. All rights reserved.
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
