52d5b54569d1a10e9272c278eb98e2775ce58680
WenmuZhou <572459439@qq.com>
2022-07-05 03:29:43 +0000

    add import cv2

--- ppocr/utils/visual.py
+++ ppocr/utils/visual.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import cv2
 import os
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
