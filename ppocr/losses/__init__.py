7054013004a9282b5e0b4354a0b689a680ff08b4
xiaoting <31891223+tink2123@users.noreply.github.com>
2022-08-12 10:49:54 +0800

    Submit SR model (#6933)
    * add sr model
    
    * update for eval
    
    * submit sr
    
    * polish code
    
    * polish code
    
    * polish code
    
    * update sr model
    
    * update doc
    
    * update doc
    
    * update doc
    
    * fix typo
    
    * format code
    
    * update metric
    
    * fix export

--- ppocr/losses/__init__.py
+++ ppocr/losses/__init__.py
@@ -57,6 +57,9 @@ from .table_master_loss import TableMasterLoss
 # vqa token loss
 from .vqa_token_layoutlm_loss import VQASerTokenLayoutLMLoss
 
+# sr loss
+from .stroke_focus_loss import StrokeFocusLoss
+
 
 def build_loss(config):
     support_dict = [
@@ -64,7 +67,7 @@ def build_loss(config):
         'ClsLoss', 'AttentionLoss', 'SRNLoss', 'PGLoss', 'CombinedLoss',
         'CELoss', 'TableAttentionLoss', 'SARLoss', 'AsterLoss', 'SDMGRLoss',
         'VQASerTokenLayoutLMLoss', 'LossFromOutput', 'PRENLoss', 'MultiLoss',
-        'TableMasterLoss', 'SPINAttentionLoss', 'VLLoss'
+        'TableMasterLoss', 'SPINAttentionLoss', 'VLLoss','StrokeFocusLoss'
     ]
     config = copy.deepcopy(config)
     module_name = config.pop('name')
