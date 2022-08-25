3416aa65c60bd4e93a6495c97718e787c6c5c036
Evezerest <50011306+Evezerest@users.noreply.github.com>
2022-08-25 18:55:46 +0800

    Merge pull request #7356 from Evezerest/dygraph
    Update PPOCRLabel.py

--- __init__.py
+++ __init__.py
@@ -17,5 +17,5 @@ __version__ = paddleocr.VERSION
 __all__ = [
     'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result',
     'save_structure_res', 'download_with_progressbar', 'sorted_layout_boxes',
-    'convert_info_docx'
+    'convert_info_docx', 'to_excel'
 ]
