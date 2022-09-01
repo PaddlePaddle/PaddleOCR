b02faa93065462418d34b96ccbefc9d18c3c4b5d
zhangxinnan <zhangxin19870504@163.com>
2022-09-02 07:04:02 +0800

    优化可视化方法，使得倾斜的文本框文字不画在框外

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -447,6 +447,65 @@ def draw_ocr_box_txt(image,
     return np.array(img_show)
 
 
+def draw_ocr_box_txt2(image,
+                      boxes,
+                      txts,
+                      scores=None,
+                      drop_score=0.5,
+                      font_path="./doc/fonts/simfang.ttf"):
+    h, w = image.height, image.width
+    img_left = image.copy()
+    img_right = np.ones((h, w, 3), dtype=np.uint8) * 255
+    import random
+
+    random.seed(0)
+    draw_left = ImageDraw.Draw(img_left)
+    for idx, (box, txt) in enumerate(zip(boxes, txts)):
+        if scores is not None and scores[idx] < drop_score:
+            continue
+        color = (random.randint(0, 255), random.randint(0, 255),
+                 random.randint(0, 255))
+        draw_left.polygon(box, fill=color)
+        img_right_text = draw_box_txt_fine((w, h), box, txt, font_path)
+        pts = np.array(box, np.int32).reshape((-1, 1, 2))
+        cv2.polylines(img_right_text, [pts], True, color, 1)
+        img_right = cv2.bitwise_and(img_right, img_right_text)
+    img_left = Image.blend(image, img_left, 0.5)
+    img_show = Image.new('RGB', (w * 2, h), (255, 255, 255))
+    img_show.paste(img_left, (0, 0, w, h))
+    img_show.paste(Image.fromarray(img_right), (w, 0, w * 2, h))
+    return np.array(img_show)
+
+
+def draw_box_txt_fine(img_size, box, txt, font_path):
+    box_height = int(math.sqrt((box[0][0] - box[3][0])**2 + (box[0][1] - box[3][1])**2))
+    box_width = int(math.sqrt((box[0][0] - box[1][0])**2 + (box[0][1] - box[1][1])**2))
+    img_text = Image.new('RGB', (box_width, box_height), (255, 255, 255))
+    draw_text = ImageDraw.Draw(img_text)
+    if box_height > 2 * box_width and box_height > 30:
+        font_size = max(int(box_width * 0.9), 10)
+        font = ImageFont.truetype(font_path, font_size, encoding="utf-8")
+        cur_y = 0
+        for c in txt:
+            draw_text.text((0, cur_y), c, fill=(0, 0, 0), font=font)
+            cur_y += font.getsize(c)[1]
+    else:
+        font_size = max(int(box_height * 0.8), 10)
+        font = ImageFont.truetype(font_path, font_size, encoding="utf-8")
+        draw_text.text([0, 0], txt, fill=(0, 0, 0), font=font)
+
+    pts1 = np.float32([[0, 0], [box_width, 0], [box_width, box_height], [0, box_height]])
+    pts2 = np.array(box, dtype=np.float32)
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+
+    img_text = np.array(img_text, dtype=np.uint8)
+    img_right_text = cv2.warpPerspective(img_text, M, img_size,
+                                         flags=cv2.INTER_NEAREST,
+                                         borderMode=cv2.BORDER_CONSTANT,
+                                         borderValue=(255, 255, 255))
+    return img_right_text
+
+
 def str_count(s):
     """
     Count the number of Chinese characters,
