1958a3de33641028b7929c95ff717be648f7c969
Arturo <donarturo1986@gmail.com>
2022-10-03 00:46:47 +0000

    Update predict_system.py
    Avoid error if installed ( Tools )
    Tested with ( tools==0.1.9 )

--- tools/infer/predict_system.py
+++ tools/infer/predict_system.py
@@ -28,13 +28,13 @@ import json
 import time
 import logging
 from PIL import Image
-import tools.infer.utility as utility
-import tools.infer.predict_rec as predict_rec
-import tools.infer.predict_det as predict_det
-import tools.infer.predict_cls as predict_cls
+import paddleocr.tools.infer.utility as utility
+import paddleocr.tools.infer.predict_rec as predict_rec
+import paddleocr.tools.infer.predict_det as predict_det
+import paddleocr.tools.infer.predict_cls as predict_cls
 from ppocr.utils.utility import get_image_file_list, check_and_read
 from ppocr.utils.logging import get_logger
-from tools.infer.utility import draw_ocr_box_txt, get_rotate_crop_image
+from paddleocr.tools.infer.utility import draw_ocr_box_txt, get_rotate_crop_image
 logger = get_logger()
 
 
