8a45f11601e40af7d1905526c90679ac35358123
WenmuZhou <572459439@qq.com>
2022-10-31 17:44:16 +0800

    Adapt to 0-dimensional tensor

--- ppocr/postprocess/rec_postprocess.py
+++ ppocr/postprocess/rec_postprocess.py
@@ -891,7 +891,7 @@ class VLLabelDecode(BaseRecLabelDecode):
             ) + length[i])].topk(1)[0][:, 0]
             preds_prob = paddle.exp(
                 paddle.log(preds_prob).sum() / (preds_prob.shape[0] + 1e-6))
-            text.append((preds_text, preds_prob.numpy()[0]))
+            text.append((preds_text, float(preds_prob)))
         if label is None:
             return text
         label = self.decode(label)
