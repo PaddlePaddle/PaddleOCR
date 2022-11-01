90859538e3f2b3ba93773652974100262520a64f
niuliling123 <sdust_niull@163.com>
2022-11-01 14:55:21 +0800

    sh

---
+++ 1_run_ocr.sh
@@ -0,0 +1,1 @@
+python -m paddle.distributed.launch --gpus=0,1,2,3 tools/train.py -c config.yml
---
+++ 2_run_ocr.sh
@@ -0,0 +1,1 @@
+python -m paddle.distributed.launch --gpus=4,5,6,7 tools/train.py -c config_o2_cast_conv.yml
