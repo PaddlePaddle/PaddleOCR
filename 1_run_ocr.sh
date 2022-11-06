34ce760a91df7de7ffddcec229a9520f22931807
niuliling123 <sdust_niull@163.com>
2022-11-06 15:19:12 +0800

    addd o1 and o2 acc500

--- 1_run_ocr.sh
+++ 1_run_ocr.sh
@@ -1,1 +1,2 @@
+export NVIDIA_TF32_OVERRIDE=0
 python -m paddle.distributed.launch --gpus=0,1,2,3 tools/train.py -c config.yml
--- 2_run_ocr.sh
+++ 2_run_ocr.sh
@@ -1,1 +1,2 @@
+export NVIDIA_TF32_OVERRIDE=0
 python -m paddle.distributed.launch --gpus=4,5,6,7 tools/train.py -c config_o2_cast_conv.yml
