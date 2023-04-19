636261b43746846526e1f5dd218e837a46f1c1b1
huangjun12 <2399845970@qq.com>
2023-04-19 07:33:22 +0000

    fix bug

--- ppocr/losses/det_db_loss.py
+++ ppocr/losses/det_db_loss.py
@@ -20,6 +20,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import paddle
 from paddle import nn
 
 from .det_basic_loss import BalanceLoss, MaskL1Loss, DiceLoss
