eb9b7c33c6300a4fea2fafe7918787c40fd8dce8
LDOUBLEV <liuvv0203@outlook.com>
2023-05-17 11:17:13 +0800

    add dml nrtr distill loss

--- ppocr/losses/distillation_loss.py
+++ ppocr/losses/distillation_loss.py
@@ -106,15 +106,8 @@ class DistillationDMLLoss(DMLLoss):
                 out2 = out2[self.key]
             if self.maps_name is None:
                 if self.multi_head:
-                    # for nrtr dml loss
-                    max_len = batch[3].max()
-                    tgt = batch[2][:, 1:2 + max_len]
-                    tgt = tgt.reshape([-1])
-                    non_pad_mask = paddle.not_equal(
-                        tgt, paddle.zeros(
-                            tgt.shape, dtype=tgt.dtype))
                     loss = super().forward(out1[self.dis_head],
-                                           out2[self.dis_head], non_pad_mask)
+                                           out2[self.dis_head])
                 else:
                     loss = super().forward(out1, out2)
                 if isinstance(loss, dict):
@@ -135,6 +128,43 @@ class DistillationDMLLoss(DMLLoss):
                     else:
                         loss_dict["{}_{}_{}".format(self.name, self.maps_name[
                             _c], idx)] = loss
+
+        loss_dict = _sum_loss(loss_dict)
+
+        return loss_dict
+
+
+class DistillationNRTRDMLLoss(DistillationDMLLoss):
+    """
+    """
+
+    def forward(self, predicts, batch):
+        loss_dict = dict()
+        for idx, pair in enumerate(self.model_name_pairs):
+            out1 = predicts[pair[0]]
+            out2 = predicts[pair[1]]
+            if self.key is not None:
+                out1 = out1[self.key]
+                out2 = out2[self.key]
+
+            if self.multi_head:
+                # for nrtr dml loss
+                max_len = batch[3].max()
+                tgt = batch[2][:, 1:2 + max_len]
+                tgt = tgt.reshape([-1])
+                non_pad_mask = paddle.not_equal(
+                    tgt, paddle.zeros(
+                        tgt.shape, dtype=tgt.dtype))
+                loss = super().forward(out1[self.dis_head], out2[self.dis_head],
+                                       non_pad_mask)
+            else:
+                loss = super().forward(out1, out2)
+            if isinstance(loss, dict):
+                for key in loss:
+                    loss_dict["{}_{}_{}_{}".format(key, pair[0], pair[1],
+                                                   idx)] = loss[key]
+            else:
+                loss_dict["{}_{}".format(self.name, idx)] = loss
 
         loss_dict = _sum_loss(loss_dict)
 
