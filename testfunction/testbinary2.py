3a83436d093c42365dd25e6693aaea19b2d6f62c
shelley hsu <shelley80812@gmail.com>
2023-05-19 16:01:17 +0800

    update testcase

--- testfunction/testbinary2.py
+++ testfunction/testbinary2.py
@@ -18,23 +18,15 @@ def color_filter(input_image):
                 hsv_image[i, j] = [0, 0, 255]
 
     output_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-<<<<<<< HEAD
     cv2.namedWindow("Original Image", cv2.WINDOW_NORMAL)
     cv2.imshow("Original Image", input_image)
     cv2.namedWindow("Filtered Image", cv2.WINDOW_NORMAL)
-=======
-    cv2.imshow("Original Image", input_image)
->>>>>>> cbd04d1fbbfe8f7d1f0ed5ccd24503c692676c55
     cv2.imshow("Filtered Image", output_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 # 读取输入图像
-<<<<<<< HEAD
 input_image = cv2.imread("../pdftoimg_main/__w__CP071-000-000038-000-000-000_001_page_2_cover.jpg")
-=======
-input_image = cv2.imread("../pdftoimg_main\__w__CP071-000-000038-000-000-000_001_page_2_cover.jpg")
->>>>>>> cbd04d1fbbfe8f7d1f0ed5ccd24503c692676c55
 
 # 进行颜色过滤处理并显示结果
 color_filter(input_image)
