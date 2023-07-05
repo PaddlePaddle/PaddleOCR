4420a5f65fb2a4799339350059a4f9e9b8e71df6
duanyanhui <45005871+YanhuiDua@users.noreply.github.com>
2023-07-05 15:41:40 +0800

    [npu-tipc] update npu tipc scripts (#10102)
    * update npu tipc scripts
    
    * update

--- test_tipc/test_train_inference_python_npu.sh
+++ test_tipc/test_train_inference_python_npu.sh
@@ -35,12 +35,20 @@ dataline=`cat $FILENAME`
 IFS=$'\n'
 lines=(${dataline})
 
+modelname=$(echo ${lines[1]} | cut -d ":" -f2)
+if  [ $modelname == "rec_r31_sar" ] || [ $modelname == "rec_mtb_nrtr" ]; then
+    sed -i "s/Global.epoch_num:lite_train_lite_infer=2/Global.epoch_num:lite_train_lite_infer=1/g" $FILENAME
+    sed -i "s/gpu_list:0|0,1/gpu_list:0,1/g" $FILENAME
+    sed -i "s/Global.use_npu:True|True/Global.use_npu:True/g" $FILENAME
+fi
+
 # replace training config file
 grep -n 'tools/.*yml' $FILENAME  | cut -d ":" -f 1 \
 | while read line_num ; do
     train_cmd=$(func_parser_value "${lines[line_num-1]}")
     trainer_config=$(func_parser_config ${train_cmd})
     sed -i 's/use_gpu/use_npu/g' "$REPO_ROOT_PATH/$trainer_config"
+    sed -i 's/use_sync_bn: True/use_sync_bn: False/g' "$REPO_ROOT_PATH/$trainer_config"
 done
 
 # change gpu to npu in execution script
