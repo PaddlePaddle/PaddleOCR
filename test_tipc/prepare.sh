9e911d4ab8c23702c667258b0662e5e9c209e54e
zhangyikun02 <48021248+zhangyk0314@users.noreply.github.com>
2023-08-01 19:04:51 +0800

    fix bug for table_master in tipc when running 'lite_train_lite_infer' mode (#10514)

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -243,6 +243,8 @@ if [ ${MODE} = "lite_train_lite_infer" ];then
     if [ ${model_name} == "table_master" ];then
         wget -nc -P ./pretrain_models/ https://paddleocr.bj.bcebos.com/ppstructure/models/tablemaster/table_structure_tablemaster_train.tar --no-check-certificate
         cd ./pretrain_models/ && tar xf table_structure_tablemaster_train.tar  && cd ../
+        wget -nc -P ./train_data/ https://paddleocr.bj.bcebos.com/dataset/StructureLabel_val_500.tar --no-check-certificate
+        cd ./train_data/ && tar xf StructureLabel_val_500.tar && cd ../
     fi
     rm -rf ./train_data/icdar2015
     rm -rf ./train_data/ic15_data
