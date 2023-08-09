b1f6c210b3778c2ae32056cba2dd79675ebd14ae
moehuster <moehuster@gmail.com>
2023-08-09 19:37:47 +0800

    update: Usinig intuitive initialization of duration (#10512)

--- deploy/cpp_infer/src/ocr_rec.cpp
+++ deploy/cpp_infer/src/ocr_rec.cpp
@@ -20,12 +20,9 @@ void CRNNRecognizer::Run(std::vector<cv::Mat> img_list,
                          std::vector<std::string> &rec_texts,
                          std::vector<float> &rec_text_scores,
                          std::vector<double> &times) {
-  std::chrono::duration<float> preprocess_diff =
-      std::chrono::steady_clock::now() - std::chrono::steady_clock::now();
-  std::chrono::duration<float> inference_diff =
-      std::chrono::steady_clock::now() - std::chrono::steady_clock::now();
-  std::chrono::duration<float> postprocess_diff =
-      std::chrono::steady_clock::now() - std::chrono::steady_clock::now();
+  std::chrono::duration<float> preprocess_diff = std::chrono::duration<float>::zero();
+  std::chrono::duration<float> inference_diff = std::chrono::duration<float>::zero();
+  std::chrono::duration<float> postprocess_diff = std::chrono::duration<float>::zero();
 
   int img_num = img_list.size();
   std::vector<float> width_list;
