bfe365fbd6d97c5f4c01f99ebf265f7ef4d9f82e
xupeng <1192152456@qq.com>
2023-08-16 18:08:05 +0800

    修改数据增强个数及顺序DSR报错

--- ppocr/data/simple_dataset.py
+++ ppocr/data/simple_dataset.py
@@ -61,7 +61,6 @@ class SimpleDataSet(Dataset):
     def set_epoch_as_seed(self, seed, dataset_config):
         if self.mode == 'train':
             try:
-
                 border_map_id = [index 
                     for index, dictionary in enumerate(dataset_config['transforms']) 
                     if 'MakeBorderMap' in dictionary
@@ -71,7 +70,6 @@ class SimpleDataSet(Dataset):
                     for index, dictionary in enumerate(dataset_config['transforms']) 
                     if 'MakeShrinkMap' in dictionary
                     ][0]
-
                 dataset_config['transforms'][border_map_id]['MakeBorderMap'][
                     'epoch'] = seed if seed is not None else 0
                 dataset_config['transforms'][shrink_map_id]['MakeShrinkMap'][
