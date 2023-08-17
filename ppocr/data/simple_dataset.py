8f010ecf1eaa1ba509c0a15f92023b32160ab491
Dec20B <1192152456@qq.com>
2023-08-17 15:32:56 +0800

    修改数据增强导致的DSR报错 (#10662)
    * 修改数据增强导致的DSR报错
    
    * 错误修改回滚

--- ppocr/data/simple_dataset.py
+++ ppocr/data/simple_dataset.py
@@ -61,9 +61,15 @@ class SimpleDataSet(Dataset):
     def set_epoch_as_seed(self, seed, dataset_config):
         if self.mode == 'train':
             try:
-                dataset_config['transforms'][5]['MakeBorderMap'][
+                border_map_id = [index
+                    for index, dictionary in enumerate(dataset_config['transforms'])
+                    if 'MakeBorderMap' in dictionary][0]
+                shrink_map_id = [index
+                    for index, dictionary in enumerate(dataset_config['transforms'])
+                    if 'MakeShrinkMap' in dictionary][0]
+                dataset_config['transforms'][border_map_id]['MakeBorderMap'][
                     'epoch'] = seed if seed is not None else 0
-                dataset_config['transforms'][6]['MakeShrinkMap'][
+                dataset_config['transforms'][shrink_map_id]['MakeShrinkMap'][
                     'epoch'] = seed if seed is not None else 0
             except Exception as E:
                 print(E)
