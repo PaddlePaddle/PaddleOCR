55819fd2275d6847918508125c8d7a4838e2491b
xu-peng-7 <1192152456@qq.com>
2023-08-17 09:35:17 +0800

    修改数据增强个数及顺序DSR报错

--- ppocr/data/simple_dataset.py
+++ ppocr/data/simple_dataset.py
@@ -61,12 +61,12 @@ class SimpleDataSet(Dataset):
     def set_epoch_as_seed(self, seed, dataset_config):
         if self.mode == 'train':
             try:
-                border_map_id = [index 
-                    for index, dictionary in enumerate(dataset_config['transforms']) 
+                border_map_id = [index
+                    for index, dictionary in enumerate(dataset_config['transforms'])
                     if 'MakeBorderMap' in dictionary
                     ][0]
-                shrink_map_id = [index 
-                    for index, dictionary in enumerate(dataset_config['transforms']) 
+                shrink_map_id = [index
+                    for index, dictionary in enumerate(dataset_config['transforms'])
                     if 'MakeShrinkMap' in dictionary
                     ][0]
                 dataset_config['transforms'][border_map_id]['MakeBorderMap'][
