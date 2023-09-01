cbc31f5ffe463ab487502c020f2e733b0b31fda8
xiezheng-XD <141627292+xiezheng-XD@users.noreply.github.com>
2023-09-01 14:35:14 +0800

    Update setup.py (#10749)

--- setup.py
+++ setup.py
@@ -15,4 +15,37 @@
 from setuptools import setup
 
 
-setup()
+def readme():
+    with open('doc/doc_en/whl_en.md', encoding="utf-8-sig") as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name='paddleocr',
+    packages=['paddleocr'],
+    package_dir={'paddleocr': ''},
+    include_package_data=True,
+    entry_points={"console_scripts": ["paddleocr= paddleocr.paddleocr:main"]},
+    version=VERSION,
+    install_requires=load_requirements(['requirements.txt', 'ppstructure/recovery/requirements.txt']),
+    license='Apache License 2.0',
+    description='Awesome OCR toolkits based on PaddlePaddle (8.6M ultra-lightweight pre-trained model, support training and deployment among server, mobile, embeded and IoT devices)',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PaddlePaddle/PaddleOCR',
+    download_url='https://github.com/PaddlePaddle/PaddleOCR.git',
+    keywords=[
+        'ocr textdetection textrecognition paddleocr crnn east star-net rosetta ocrlite db chineseocr chinesetextdetection chinesetextrecognition'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers', 'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7', 'Topic :: Utilities'
+    ], )
