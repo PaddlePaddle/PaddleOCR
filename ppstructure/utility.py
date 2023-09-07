b98e42cb77066a6c4c6ebc17e20a264619988bac
ToddBear <43341135+ToddBear@users.noreply.github.com>
2023-09-07 15:44:20 +0800

    Update utility.py

--- ppstructure/utility.py
+++ ppstructure/utility.py
@@ -16,7 +16,7 @@ import ast
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from tools.infer.utility import draw_ocr_box_txt, str2bool, str2int_tuple, init_args as infer_args
-
+import math
 
 def init_args():
     parser = infer_args()
