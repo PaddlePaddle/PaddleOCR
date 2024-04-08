d892d6cd0a2ca16abddf713bea2d3ee68d0c1bda
Sanjay Rijal <37138338+zovelsanj@users.noreply.github.com>
2024-04-08 13:20:22 +0545

    handle triangular contour exceptions
    In case any two coordinates of a contour are same, it will make a triangular region instead of rectangular one. Thus, when creating polygon from that contour, the area of the box (and thus distance) will be zero, resulting in an empty box. This results the following error during minimum area calculation:
    ```
    bounding_box = cv2.minAreaRect(contour)
    cv2.error: OpenCV(4.6.0) D:\a\opencv-python\opencv-python\opencv\modules\imgproc\src\convhull.cpp:143: error: (-215:Assertion failed) total >= 0 && (depth == CV_32F || depth == CV_32S) in function 'cv::convexHull'
    ```

--- ppocr/postprocess/db_postprocess.py
+++ ppocr/postprocess/db_postprocess.py
@@ -87,6 +87,8 @@ class DBPostProcess(object):
             else:
                 continue
             box = box.reshape(-1, 2)
+            if len(box) == 0:
+                continue
 
             _, sside = self.get_mini_boxes(box.reshape((-1, 1, 2)))
             if sside < self.min_size + 2:
