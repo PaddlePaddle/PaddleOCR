9baed76cad141f54031052c6f88404979cdce5c5
Liyulingyue <852433440@qq.com>
2024-05-23 21:26:28 +0800

    fix version

--- __init__.py
+++ __init__.py
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .paddleocr import *
+import importlib.metadata as importlib_metadata
 
-__version__ = paddleocr.VERSION
+try:
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 __all__ = [
     "PaddleOCR",
     "PPStructure",
