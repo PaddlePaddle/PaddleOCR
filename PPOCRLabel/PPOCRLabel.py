9b41d60087eb6cc0e4fdb285e31dc330f2fd1562
fffreedom <fffreedom@users.noreply.github.com>
2024-06-13 16:49:30 +0800

    func setValue of bar only accept an int parameter
    fix: use int parameter for bar.setValue in scrollRequest

--- PPOCRLabel/PPOCRLabel.py
+++ PPOCRLabel/PPOCRLabel.py
@@ -1422,7 +1422,7 @@ class MainWindow(QMainWindow):
     def scrollRequest(self, delta, orientation):
         units = - delta / (8 * 15)
         bar = self.scrollBars[orientation]
-        bar.setValue(bar.value() + bar.singleStep() * units)
+        bar.setValue(int(bar.value() + bar.singleStep() * units))
 
     def setZoom(self, value):
         self.actions.fitWidth.setChecked(False)
