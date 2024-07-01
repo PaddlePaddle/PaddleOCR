be1bcff2b82a0d6d585062474dce411b33da9062
Christine Straub <christinemstraub@gmail.com>
2024-07-01 13:02:47 -0700

    chore: sync version to upstream main branch

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     name="unstructured.paddleocr",
     packages=find_packages(),
     include_package_data=True,
-    version="2.7.5",
+    version="2.8.0",
     install_requires=[
         "shapely",
         "scikit-image",
