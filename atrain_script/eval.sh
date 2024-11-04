892793e3242ec1f4fa1242764b56a2c9fc9aa06e
svtter <svtter@qq.com>
2024-11-04 15:47:53 +0800

    validation passwd with docker compose. Feat.

--- atrain_script/eval.sh
+++ atrain_script/eval.sh
@@ -3,7 +3,7 @@
 
 # eval data/ folder
 
-pdm run python tools/eval.py -c configs/rec/PP-OCRv3/ch_PP-OCRv3_rec_distillation.yml \
+python tools/eval.py -c configs/rec/PP-OCRv3/ch_PP-OCRv3_rec_distillation.yml \
                          -o Global.checkpoints=ckpt/ch_PP-OCRv3_rec_train/best_accuracy \
                          Eval.dataset.data_dir=./data \
                          Eval.dataset.label_file_list=["./data/val.list"]
