7cc9be697ba7edd56fc53da4f31e29cca97e2320
svtter <svtter@qq.com>
2024-11-06 11:33:47 +0800

    use p100

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 # FROM paddlecloud/paddleocr:2.6-gpu-cuda10.2-cudnn7-latest
 
 # build from paddle
-FROM paddlepaddle/paddle:2.6.2-gpu-cuda12.0-cudnn8.9-trt8.6
+# FROM paddlepaddle/paddle:2.6.2-gpu-cuda12.0-cudnn8.9-trt8.6
+FROM svtter/paddle:cuda11.2-paddle2.4.2
 
 COPY requirements.txt .
 RUN pip install jupyterlab && pip install -r requirements.txt
--- atrain_script/train_meter_v3.sh
+++ atrain_script/train_meter_v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # train from bare empty model, with no weight.
 
-python -m paddle.distributed.launch --gpus '0,1' \
+python -m paddle.distributed.launch --gpus '4,5,6,7' \
                        tools/train.py -c ./atrain_script/configs/ch_PP-OCRv3_rec_distillation.yml \
                        -o Global.epoch_num=20 \
                        Global.eval_batch_step='[1000, 2000]' \
