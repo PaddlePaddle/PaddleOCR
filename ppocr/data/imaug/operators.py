4ee31647f30075602c80896c153fbe3456c6fcd0
Christian Clauss <cclauss@me.com>
2024-11-12 02:36:32 +0100

    Update operators.py

--- ppocr/data/imaug/operators.py
+++ ppocr/data/imaug/operators.py
@@ -38,9 +38,7 @@ class DecodeImage(object):
 
     def __call__(self, data):
         img = data["image"]
-        assert (
-            type(img) is bytes and len(img) > 0
-        ), "invalid input 'img' in DecodeImage"
+        assert type(img) is bytes and len(img) > 0, "invalid input 'img' in DecodeImage"
         img = np.frombuffer(img, dtype="uint8")
         if self.ignore_orientation:
             img = cv2.imdecode(img, cv2.IMREAD_IGNORE_ORIENTATION | cv2.IMREAD_COLOR)
