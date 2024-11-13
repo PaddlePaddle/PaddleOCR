50f3f52550f179c44c79412fc1761fa5b28d7fcf
Coobiw <bowenqu@stu.pku.edu.cn>
2024-11-13 13:47:52 +0800

    fix: Title text partially missing issue in `recovery_to_markdown.py`

--- ppstructure/recovery/recovery_to_markdown.py
+++ ppstructure/recovery/recovery_to_markdown.py
@@ -157,11 +157,9 @@ def convert_info_markdown(res, save_folder, img_name):
                 f"""<div align="center">\n\t<img src="{img_name+"/"+img_file_name}">\n</div>"""
             )
         elif region["type"].lower() == "title":
-            sorted_regions = sorted(region['res'], key=lambda x: (x["text_region"][0][1], x["text_region"][0][0]))
-            
             markdown_string.append(
-                f"""# {sorted_regions[0]['text']}""" + \
-                ''.join([" " + sorted_region['text'] for sorted_region in sorted_regions[1:]])
+                f"""# {region['res'][0]['text']}""" + \
+                ''.join([" " + one_region['text'] for one_region in region['res'][1:]])
             )
         elif region["type"].lower() == "table":
             markdown_string.append(region["res"]["html"])
