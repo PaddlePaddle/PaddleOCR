530b3c97e5347b32e032621adc80f856667c9102
svtter <svtter@qq.com>
2025-02-08 17:20:06 +0800

    merge eval meter to eval.py

--- atrain_script/eval.py
+++ atrain_script/eval.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 
 def judge_path():
+  """判断文件是否在PaddleOCR目录下"""
   current_folder = os.getcwd()
   if not current_folder.endswith("PaddleOCR"):
     print("Please run this script in the PaddleOCR folder")
@@ -17,6 +18,9 @@ def prehook(hooks: list):
 
   return wrapper
 
+def join_cmdlist(cmd_list: list):
+  return " ".join(cmd_list)
+
 
 @prehook(hooks=[judge_path])
 def eval_date():
@@ -26,17 +30,26 @@ def eval_date():
   data_dir = "./data"
   label_file_list = ["./data/val.list"]
 
-  cmd_list = [
+  cmd = join_cmdlist([
     f"python {eval_file} -c {config_file}",
     f"-o Global.checkpoints={ckpt_file}",
     f"Eval.dataset.data_dir={data_dir}",
     f"Eval.dataset.label_file_list={label_file_list}",
-  ]
-
-  cmd = " ".join(cmd_list)
+  ])
   print("running: ", cmd)
   os.system(cmd)
 
+
+def eval_meter():
+  cmd = join_cmdlist([
+    "python tools/eval.py",
+    "-c configs/rec/PP-OCRv3/ch_PP-OCRv3_rec_distillation.yml",
+    "-o Global.checkpoints=ckpt/ch_PP-OCRv3_rec_train/best_accuracy",
+    "Eval.dataset.data_dir=./train_data",
+    "Eval.dataset.label_file_list=[./train_data/rec/val.list]",
+  ])
+  print("running: ", cmd)
+  os.system(cmd)
 
 if __name__ == "__main__":
   eval_date()
