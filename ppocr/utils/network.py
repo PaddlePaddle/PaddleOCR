2f235dde3749961b433b8fe5ccd581f4263428d4
mauryaland <amaury@fouret.org>
2025-02-14 15:58:45 +0000

    use PADDLE_OCR_BASE_DIR env variable to download models

--- ppocr/utils/network.py
+++ ppocr/utils/network.py
@@ -24,9 +24,8 @@ from tqdm import tqdm
 
 from ppocr.utils.logging import get_logger
 
-MODELS_DIR = (
-    os.environ.get("PADDLE_OCR_BASE_DIR", os.path.expanduser("~/.paddleocr/"))
-    + "/models/"
+MODELS_DIR = os.path.join(
+    os.environ.get("PADDLE_OCR_BASE_DIR", os.path.expanduser("~/.paddleocr/")), "models"
 )
 DOWNLOAD_RETRY_LIMIT = 3
 
