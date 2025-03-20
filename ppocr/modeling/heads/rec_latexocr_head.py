845212c62d83ce0fc588d3a5982c1a12b6a11796
liuhongen1234567 <2998388548@qq.com>
2025-03-20 08:44:23 +0000

    fix onnx in latexocr

--- ppocr/modeling/heads/rec_latexocr_head.py
+++ ppocr/modeling/heads/rec_latexocr_head.py
@@ -907,14 +907,7 @@ class LaTeXOCRHead(nn.Layer):
             x = out[:, -self.max_seq_len :]
             mask = mask[:, -self.max_seq_len :]
             logits = self.net(x, mask=mask, **kwargs)[:, -1, :]
-            if filter_logits_fn in {top_k, top_p}:
-                filtered_logits = filter_logits_fn(logits, thres=filter_thres)
-
-                probs = F.softmax(filtered_logits / temperature, axis=-1)
-            else:
-                raise NotImplementedError("The filter_logits_fn is not supported ")
-
-            sample = paddle.multinomial(probs, 1)
+            sample = paddle.argmax(logits, axis=1).reshape([-1, 1])
             out = paddle.concat((out, sample), axis=-1)
             pad_mask = paddle.full(shape=[mask.shape[0], 1], fill_value=1, dtype="bool")
             mask = paddle.concat((mask, pad_mask), axis=1)
@@ -966,12 +959,7 @@ class LaTeXOCRHead(nn.Layer):
             logits = self.net(x, mask=mask, context=context, seq_len=i_idx, **kwargs)[
                 :, -1, :
             ]
-            if filter_logits_fn in {top_k, top_p}:
-                filtered_logits = filter_logits_fn(logits, thres=filter_thres)
-
-                probs = F.softmax(filtered_logits / temperature, axis=-1)
-
-            sample = paddle.multinomial(probs, 1)
+            sample = paddle.argmax(logits, axis=1).reshape([-1, 1])
             out = paddle.concat((out, sample), axis=-1)
 
             pad_mask = paddle.full(shape=[mask.shape[0], 1], fill_value=1, dtype="bool")
