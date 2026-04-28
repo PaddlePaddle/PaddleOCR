2f491c2133421fd332147e6e3cc48198e38d77d2
hinotoi-agent <paperlantern.agent@gmail.com>
2026-04-28 12:26:05 +0800

    test: restore YAML stubs after tests

--- tests/test_safe_yaml_config_loading.py
+++ tests/test_safe_yaml_config_loading.py
@@ -9,45 +9,56 @@ import yaml
 REPO_ROOT = Path(__file__).resolve().parents[1]
 
 
-def _stub_module(name, **attrs):
+def _stub_module(monkeypatch, name, **attrs):
     module = types.ModuleType(name)
     for key, value in attrs.items():
         setattr(module, key, value)
-    sys.modules[name] = module
+    monkeypatch.setitem(sys.modules, name, module)
     return module
 
 
-def _load_tools_program_module():
-    _stub_module("paddle")
-    _stub_module("paddle.distributed")
-    _stub_module("cv2")
-    _stub_module("numpy")
-    _stub_module("tqdm", tqdm=lambda *args, **kwargs: None)
-    _stub_module("ppocr")
-    _stub_module("ppocr.utils")
-    _stub_module("ppocr.utils.stats", TrainingStats=object)
-    _stub_module("ppocr.utils.save_load", save_model=lambda *args, **kwargs: None)
+def _load_tools_program_module(monkeypatch):
+    _stub_module(monkeypatch, "paddle")
+    _stub_module(monkeypatch, "paddle.distributed")
+    _stub_module(monkeypatch, "cv2")
+    _stub_module(monkeypatch, "numpy")
+    _stub_module(monkeypatch, "tqdm", tqdm=lambda *args, **kwargs: None)
+    _stub_module(monkeypatch, "ppocr")
+    _stub_module(monkeypatch, "ppocr.utils")
+    _stub_module(monkeypatch, "ppocr.utils.stats", TrainingStats=object)
     _stub_module(
+        monkeypatch, "ppocr.utils.save_load", save_model=lambda *args, **kwargs: None
+    )
+    _stub_module(
+        monkeypatch,
         "ppocr.utils.utility",
         print_dict=lambda *args, **kwargs: None,
         AverageMeter=object,
     )
-    _stub_module("ppocr.utils.logging", get_logger=lambda *args, **kwargs: None)
-    _stub_module("ppocr.utils.loggers", WandbLogger=object, Loggers=object)
-    _stub_module("ppocr.utils.profiler")
-    _stub_module("ppocr.data", build_dataloader=lambda *args, **kwargs: None)
-    _stub_module("ppocr.utils.export_model", export=lambda *args, **kwargs: None)
+    _stub_module(
+        monkeypatch, "ppocr.utils.logging", get_logger=lambda *args, **kwargs: None
+    )
+    _stub_module(monkeypatch, "ppocr.utils.loggers", WandbLogger=object, Loggers=object)
+    _stub_module(monkeypatch, "ppocr.utils.profiler")
+    _stub_module(
+        monkeypatch, "ppocr.data", build_dataloader=lambda *args, **kwargs: None
+    )
+    _stub_module(
+        monkeypatch, "ppocr.utils.export_model", export=lambda *args, **kwargs: None
+    )
 
+    module_name = "paddleocr_tools_program"
     spec = importlib.util.spec_from_file_location(
-        "paddleocr_tools_program", REPO_ROOT / "tools" / "program.py"
+        module_name, REPO_ROOT / "tools" / "program.py"
     )
     module = importlib.util.module_from_spec(spec)
+    monkeypatch.setitem(sys.modules, module_name, module)
     spec.loader.exec_module(module)
     return module
 
 
-def test_tools_program_load_config_rejects_python_object_tags(tmp_path):
-    module = _load_tools_program_module()
+def test_tools_program_load_config_rejects_python_object_tags(tmp_path, monkeypatch):
+    module = _load_tools_program_module(monkeypatch)
     payload = (
         '!!python/object/apply:os.system ["echo SHOULD_NOT_RUN > '
         '/tmp/paddleocr_tools_program_test"]\n'
@@ -59,8 +70,8 @@ def test_tools_program_load_config_rejects_python_object_tags(tmp_path):
         module.load_config(str(config_path))
 
 
-def test_tools_program_parse_opt_rejects_python_object_tags():
-    parser = _load_tools_program_module().ArgsParser()
+def test_tools_program_parse_opt_rejects_python_object_tags(monkeypatch):
+    parser = _load_tools_program_module(monkeypatch).ArgsParser()
     malicious_opt = [
         'Global.debug=!!python/object/apply:os.system ["echo SHOULD_NOT_RUN > '
         '/tmp/paddleocr_opt_test"]'
